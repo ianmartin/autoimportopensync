0c1ecd026de8a9d72a127789161a359cb5dc7b75
birme <birme>
2004-12-03 06:46:53 +0000

    Fixed a minor compile error bug. Forgot the char before *new_anchor.

--- src/opensync_anchor.h
+++ src/opensync_anchor.h
@@ -1,2 +1,2 @@
-osync_bool osync_anchor_compare(OSyncMember *member, char *objtypechar, *new_anchor);
+osync_bool osync_anchor_compare(OSyncMember *member, char *objtypechar, char *new_anchor);
 void osync_anchor_update(OSyncMember *member, char *objtype, char *new_anchor);
