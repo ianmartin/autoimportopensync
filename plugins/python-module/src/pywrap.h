df0df7b1a11dd3a4260fb12bdce2827c31d680c9
ehabkost <ehabkost>
2005-01-09 21:31:27 +0000

    Experimental pyrex code for wrapping opensync types

---
+++ plugins/python-module/src/pywrap.h
@@ -0,0 +1,8 @@
+#ifndef _OSYNC_PYWRAP_H_
+#define _OSYNC_PYWRAP_H_
+
+#include <opensync/opensync.h>
+#define osync_member_from_void(v) ((OSyncMember*)(m))
+#define osync_context_from_void(v) ((OSyncContext*)(m))
+
+#endif /* _OSYNC_PYWRAP_H_ */
