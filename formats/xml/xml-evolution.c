dcf9d6616c693aa5419234d8db34589dc2344210
abauer <abauer>
2005-01-26 20:13:57 +0000

    Added more copyright disclaimers

--- formats/xml/xml-evolution.c
+++ formats/xml/xml-evolution.c
@@ -1,3 +1,23 @@
+/*
+ * x-evo - A plugin for evolution extensions for the opensync framework
+ * Copyright (C) 2004-2005  Armin Bauer <armin.bauer@opensync.org>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 
+ */
+ 
 #include "xml.h"
 
 static osync_bool conv_x_evo_to_xml(const char *input, int inpsize, char **output, int *outpsize, OSyncError **error)
