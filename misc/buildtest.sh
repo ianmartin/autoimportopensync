55de9051bb8ed0c6b3f60aed56a7d8fea30ebe31
abauer <abauer>
2005-03-22 18:18:38 +0000

    Fixed the buildtest script

--- misc/buildtest.sh
+++ misc/buildtest.sh
@@ -4,9 +4,9 @@ echo -n "Cleaning Stuff now"
 
 rm -rf _inst > /dev/null
 echo -n "."
-rm -rf opensync-0.?? > /dev/null
+rm -rf libopensync-0.?? > /dev/null
 echo -n "."
-rm -f opensync-0.??.tar.gz > /dev/null
+rm -f libopensync-0.??.tar.gz > /dev/null
 echo -n "."
 echo ""
 
@@ -22,12 +22,12 @@ echo "Making tarball"
 make dist > /dev/null || exit 1
 
 mkdir _inst > /dev/null
-rm -rf opensync-0.??
+rm -rf libopensync-0.??
 
 echo "Unpacking tarball"
-tar zxvf opensync-0.??.tar.gz > /dev/null || exit 1
+tar zxvf libopensync-0.??.tar.gz > /dev/null || exit 1
 
-cd opensync-0.??
+cd libopensync-0.??
 
 echo -n "Making OpenSync"
 ./autogen.sh --prefix=$TOP_SRCDIR/_inst > /dev/null || exit 1
@@ -44,7 +44,7 @@ echo -n "."
 make install > /dev/null || exit 1
 echo "."
 
-cd $TOP_SRCDIR/opensync-0.?? || exit 1
+cd $TOP_SRCDIR/libopensync-0.?? || exit 1
 
 echo "Running tests now."
 make check || exit 1
@@ -52,6 +52,6 @@ make check || exit 1
 cd $TOP_SRCDIR
 
 rm -rf _inst
-rm -rf opensync-0.??
+rm -rf libopensync-0.??
 
 echo "Everyting went fine!"
