8872d609febcd6558ff7029eaed3bbce0c152b78
canbus <canbus>
2005-07-07 21:46:55 +0000

    Fixed a "bug" where TYPE= parameters were generated even for 2.1 vcards (optional but not required)

--- formats/vformats-xml/vformat.c
+++ formats/vformats-xml/vformat.c
@@ -693,9 +693,11 @@ char *vformat_to_string (VFormat *evc, VFormatType type)
 			if (!g_ascii_strcasecmp (param->name, "CHARSET") && type == VFORMAT_CARD_30)
 				continue;
 			attr_str = g_string_append_c (attr_str, ';');
-			attr_str = g_string_append (attr_str, param->name);
+			if (g_ascii_strcasecmp (param->name, "TYPE") || type == VFORMAT_CARD_30 || VFORMAT_TODO_20 || VFORMAT_EVENT_20)
+				attr_str = g_string_append (attr_str, param->name);
 			if (param->values) {
-				attr_str = g_string_append_c (attr_str, '=');
+				if (g_ascii_strcasecmp (param->name, "TYPE") || type == VFORMAT_CARD_30 || VFORMAT_TODO_20 || VFORMAT_EVENT_20)
+					attr_str = g_string_append_c (attr_str, '=');
 				for (v = param->values; v; v = v->next) {
 					char *value = v->data;
 					char *p = value;
