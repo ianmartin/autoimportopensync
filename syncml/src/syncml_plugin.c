3baf9d98dab6a391bf3f296979db2c62821801ea
canbus <canbus>
2005-07-08 20:59:11 +0000

    Integrated the syncml library into the syncml plugin

--- syncml/src/syncml_plugin.c
+++ syncml/src/syncml_plugin.c
@@ -1,85 +1,210 @@
-#include <opensync/opensync.h>
+/*
+ * syncml plugin - A syncml plugin for OpenSync
+ * Copyright (C) 2005  Armin Bauer <armin.bauer@opensync.org>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * 
+ */
+
 #include "syncml_plugin.h"
 
-static void *initialize(OSyncMember *member, OSyncError **error)
+
+void _send_success(void *userData, SmlTransportResult result, SmlError **error)
 {
-	char *configdata;
-	int configsize;
+	osync_trace(TRACE_ENTRY, "%s(%p, %i, %p)", __func__, userData, result, error);
+
+	osync_trace(TRACE_EXIT, "%s", __func__);
+}
+
+void _recv_final(SmlSession *session, void *userdata)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p)", __func__, session, userdata);
 	
-	//You need to specify the <some name>_environment somewhere with
-	//all the members you need
-	plugin_environment *env = malloc(sizeof(plugin_environment));
+	SmlError *error = NULL;
+	smlSessionEndPackage(session, _send_success, NULL, &error);
 	
-	//now you can get the config file for this plugin
-	if (!osync_member_get_config(member, &configdata, &configsize, error)) {
-		osync_error_update(error, "Unable to get config data: %s", osync_error_print(error));
-		free(env);
-		return NULL;
+	osync_trace(TRACE_EXIT, "%s", __func__);
+}
+
+SmlBool _new_session(SmlTransport *tsp, SmlSession *session, void *userdata, SmlError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p, %p, %p)", __func__, tsp, session, userdata, error);
+	SmlPluginEnv *env = (SmlPluginEnv *)userdata;
+	
+	if (!smlDsServerRegister(env->contactserver, session, error))
+		goto error;
+	
+	smlSessionRegisterFinalHandler(session, _recv_final, userdata);
+		
+	if (!smlAuthRegister(env->auth, session, error))
+		goto error;
+	
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+
+error:
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, smlErrorPrint(error));
+	return FALSE;
+}
+
+osync_bool syncml_http_server_parse_config(SmlTransportHttpServerConfig *env, const char *config, unsigned int size, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p, %i, %p)", __func__, env, config, size, error);
+	xmlDocPtr doc = NULL;
+	xmlNodePtr cur = NULL;
+
+	env->port = 8080;
+
+	if (!(doc = xmlParseMemory(config, size))) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Could not parse config");
+		goto error;
+	}
+
+	if (!(cur = xmlDocGetRootElement(doc))) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "config seems to be empty");
+		goto error_free_doc;
 	}
 	
-	//Process the configdata here and set the options on your environment
-	free(configdata);
+	if (xmlStrcmp(cur->name, (xmlChar*)"config")) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "config does not seem to be valid");
+		goto error_free_doc;
+	}
+
+	cur = cur->xmlChildrenNode;
+
+	while (cur != NULL) {
+		char *str = (char*)xmlNodeGetContent(cur);
+		if (str) {
+			if (!xmlStrcmp(cur->name, (const xmlChar *)"port")) {
+				env->port = atoi(str);
+			}
+			
+			if (!xmlStrcmp(cur->name, (const xmlChar *)"url")) {
+				env->url = g_strdup(str);
+			}
+			
+			if (!xmlStrcmp(cur->name, (const xmlChar *)"interface")) {
+				env->interface = g_strdup(str);
+			}
+			xmlFree(str);
+		}
+		cur = cur->next;
+	}
+
+	xmlFreeDoc(doc);
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+
+error_free_doc:
+	xmlFreeDoc(doc);
+error:
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));
+	return FALSE;
+}
+
+static void *syncml_http_server_init(OSyncMember *member, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p)", __func__, member, error);
+	char *configdata = NULL;
+	int configsize = 0;
+	SmlError *serror = NULL;
+	SmlTransportHttpServerConfig serverConfig;
+	
+	SmlPluginEnv *env = osync_try_malloc0(sizeof(SmlPluginEnv), error);
+	if (!env)
+		goto error;
+		
+	if (!osync_member_get_config(member, &configdata, &configsize, error))
+		goto error_free_env;
+	
+	env->tsp = smlTransportNew(SML_TRANSPORT_HTTP_SERVER, &serror);
+	if (!env->tsp)
+		goto error_free_config;
+		
+	smlTransportSetNewSessionCallback(env->tsp, _new_session, NULL);
+	
+	if (!syncml_http_server_parse_config(&serverConfig, configdata, configsize, error))
+		goto error_free_transport;
+	
+	env->context = osync_member_get_loop(member);
 	env->member = member;
 	
-	//If you need a hashtable you make it here
-	env->hashtable = osync_hashtable_new();
+	if (!smlTransportInitialize(env->tsp, &serverConfig, env->context, &serror))
+		goto error_free_transport;
 	
-	//Now your return your struct.
+	SmlLocation *loc = smlLocationNew("/vcards", NULL, &serror);
+	
+	env->contactserver = smlDsServerNew(loc, &serror);
+	if (!env->contactserver)
+		goto error_free_loc;
+	
+	env->auth = smlAuthNew(&serror);
+	if (!env->auth)
+		goto error_free_contactserver;
+
+	
+	g_free(configdata);
+	
+	osync_trace(TRACE_EXIT, "%s: %p", __func__, env);
 	return (void *)env;
+error_free_contactserver:
+	smlDsServerFree(env->contactserver);
+error_free_loc:
+	smlLocationFree(loc);
+error_free_transport:
+	smlTransportFree(env->tsp);
+error_free_config:
+	g_free(configdata);
+error_free_env:
+	g_free(env);
+error:
+	if (serror)
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "%s", smlErrorPrint(&serror));
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));
+	return NULL;
 }
 
 static void connect(OSyncContext *ctx)
 {
-	//Each time you get passed a context (which is used to track
-	//calls to your plugin) you can get the data your returned in
-	//initialize via this call:
-	plugin_environment *env = (plugin_environment *)osync_context_get_plugin_data(ctx);
+	osync_trace(TRACE_ENTRY, "%s(%p)", __func__, ctx);
+	SmlPluginEnv *env = (SmlPluginEnv *)osync_context_get_plugin_data(ctx);
 
-	/*
-	 * Now connect to your devices and report
-	 * 
-	 * an error via:
-	 * osync_context_report_error(ctx, ERROR_CODE, "Some message");
-	 * 
-	 * or success via:
-	 * osync_context_report_success(ctx);
-	 * 
-	 * You have to use one of these 2 somewhere to answer the context.
-	 * 
-	 */
-	
-	//If you are using a hashtable you have to load it here
-	OSyncError *error = NULL;
-	if (!osync_hashtable_load(env->hashtable, env->member, &error)) {
-		osync_context_report_osyncerror(ctx, &error);
-		return;
-	}
-	
+
 	//you can also use the anchor system to detect a device reset
 	//or some parameter change here. Check the docs to see how it works
 	char *lanchor = NULL;
 	//Now you get the last stored anchor from the device
 	if (!osync_anchor_compare(env->member, "lanchor", lanchor))
-		osync_member_set_slow_sync(env->member, "<object type to request a slow-sync>", TRUE);
+		osync_member_set_slow_sync(env->member, "contact", TRUE);
+		
+	osync_context_report_success(ctx);
 }
 
 static void get_changeinfo(OSyncContext *ctx)
 {
-	plugin_environment *env = (plugin_environment *)osync_context_get_plugin_data(ctx);
+	SmlPluginEnv *env = (SmlPluginEnv *)osync_context_get_plugin_data(ctx);
 	
-	//If you use opensync hashtables you can detect if you need
-	//to do a slow-sync and set this on the hastable directly
-	//otherwise you have to make 2 function like "get_changes" and
-	//"get_all" and decide which to use using
-	//osync_member_get_slow_sync
-	if (osync_member_get_slow_sync(env->member, "<object type>"))
-		osync_hashtable_set_slow_sync(env->hashtable, "<object type>");
+	if (osync_member_get_slow_sync(env->member, "contact")) {
+		;
+	}
 
 	/*
 	 * Now you can get the changes.
 	 * Loop over all changes you get and do the following:
 	 */
-		char *data = NULL;
+	/*	char *data = NULL;
 		//Now get the data of this change
 		
 		//Make the new change to report
@@ -96,68 +221,30 @@ static void get_changeinfo(OSyncContext *ctx)
 		//Set the last argument to FALSE if the real data
 		//should be queried later in a "get_data" function
 		
-		osync_change_set_data(change, data, sizeof(data), TRUE);			
+		osync_change_set_data(change, data, sizeof(data), TRUE);		*/	
 
-		//If you use hashtables use these functions:
-		if (osync_hashtable_detect_change(env->hashtable, change)) {
-			osync_context_report_change(ctx, change);
-			osync_hashtable_update_hash(env->hashtable, change);
-		}	
 		//otherwise just report the change via
 		//osync_context_report_change(ctx, change);
 
-	//When you are done looping and if you are using hashtables	
-	osync_hashtable_report_deleted(env->hashtable, ctx, "data");
-	
 	//Now we need to answer the call
 	osync_context_report_success(ctx);
 }
 
-static osync_bool commit_change(OSyncContext *ctx, OSyncChange *change)
+static void batch_commit(OSyncContext *ctx, OSyncContext **contexts, OSyncChange **changes)
 {
-	plugin_environment *env = (plugin_environment *)osync_context_get_plugin_data(ctx);
+	osync_trace(TRACE_ENTRY, "%s(%p, %p, %p)", __func__, ctx, contexts, changes);
+	SmlPluginEnv *env = (SmlPluginEnv *)osync_context_get_plugin_data(ctx);
 	
-	/*
-	 * Here you have to add, modify or delete a object
-	 * 
-	 */
-	switch (osync_change_get_changetype(change)) {
-		case CHANGE_DELETED:
-			//Delete the change
-			//Dont forget to answer the call on error
-			break;
-		case CHANGE_ADDED:
-			//Add the change
-			//Dont forget to answer the call on error
-			//If you are using hashtables you have to calculate the hash here:
-			osync_change_set_hash(change, "new hash");
-			break;
-		case CHANGE_MODIFIED:
-			//Modify the change
-			//Dont forget to answer the call on error
-			//If you are using hashtables you have to calculate the new hash here:
-			osync_change_set_hash(change, "new hash");
-			break;
-		default:
-			osync_debug("FILE-SYNC", 0, "Unknown change type");
-	}
-	//Answer the call
-	osync_context_report_success(ctx);
-	//if you use hashtable, update the hash now.
-	osync_hashtable_update_hash(env->hashtable, change);
-	return TRUE;
+	osync_trace(TRACE_EXIT, "%s: %p", __func__, env);
 }
 
 static void sync_done(OSyncContext *ctx)
 {
-	plugin_environment *env = (plugin_environment *)osync_context_get_plugin_data(ctx);
+	SmlPluginEnv *env = (SmlPluginEnv *)osync_context_get_plugin_data(ctx);
 	
 	/*
 	 * This function will only be called if the sync was successfull
 	 */
-	
-	//If we have a hashtable we can now forget the already reported changes
-	osync_hashtable_forget(env->hashtable);
 	
 	//If we use anchors we have to update it now.
 	char *lanchor = NULL;
@@ -170,54 +257,35 @@ static void sync_done(OSyncContext *ctx)
 
 static void disconnect(OSyncContext *ctx)
 {
-	plugin_environment *env = (plugin_environment *)osync_context_get_plugin_data(ctx);
+	//SmlPluginEnv *env = (SmlPluginEnv *)osync_context_get_plugin_data(ctx);
 	
 	//Close all stuff you need to close
 	
-	//Close the hashtable
-	osync_hashtable_close(env->hashtable);
 	//Answer the call
 	osync_context_report_success(ctx);
 }
 
 static void finalize(void *data)
 {
-	plugin_environment *env = (plugin_environment *)data;
-	//Free all stuff that you have allocated here.
-	osync_hashtable_free(env->hashtable);
+	SmlPluginEnv *env = (SmlPluginEnv *)data;
+	g_free(env);
 }
 
-void get_info(OSyncPluginInfo *info)
+void get_info(OSyncEnv *env)
 {
-	//Tell opensync something about your plugin
-	info->name = "short name, maybe < 15 chars";
-	info->longname = "long name. maybe < 50 chars";
-	info->description = "A longer description. < 200 chars";
-	//the version of the api we are using, (1 at the moment)
-	info->version = 1;
-	//If you plugin is threadsafe.
-	info->is_threadsafe = TRUE;
+	OSyncPluginInfo *info = osync_plugin_new_info(env);
 	
-	//Now set the function we made earlier
-	info->functions.initialize = initialize;
+	info->name = "syncml-http-server";
+	info->longname = "SyncML over HTTP Server";
+	
+	info->functions.initialize = syncml_http_server_init;
 	info->functions.connect = connect;
 	info->functions.sync_done = sync_done;
 	info->functions.disconnect = disconnect;
 	info->functions.finalize = finalize;
 	info->functions.get_changeinfo = get_changeinfo;
 	
-	//If you like, you can overwrite the default timeouts of your plugin
-	//The default is set to 60 sec. Note that this MUST NOT be used to
-	//wait for expected timeouts (Lets say while waiting for a webserver).
-	//you should wait for the normal timeout and return a error.
-	info->timeouts.connect_timeout = 5;
-	//There are more timeouts for the other functions
-	
-	//Now you have to tell opensync all the object types that your are accepting.
-	//This can be more than one
-	osync_plugin_accept_objtype(info, "<object type name>");
-	//which format do you accept for this objtype
-	osync_plugin_accept_objformat(info, "<object type name>", "<format name>", "<name of the required extension if any>");
-	//set the commit function for this format
-	osync_plugin_set_commit_objformat(info, "<object type name>", "<format name>", commit_change);
+	osync_plugin_accept_objtype(info, "contact");
+	osync_plugin_accept_objformat(info, "contact", "vcard21", NULL);
+	osync_plugin_set_batch_commit_objformat(info, "contact", "vcard21", batch_commit);
 }
