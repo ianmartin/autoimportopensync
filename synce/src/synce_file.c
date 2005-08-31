864bb062e4f16fa3daccb1636cd5dbdda6526e0a
dbackx <dbackx>
2005-08-31 15:16:36 +0000

    Use advice given by Armin, use fileFormat struct in <opensync/file.h>, clean up the code a bit, and ... this is the first version to actually work !

--- synce/src/synce_file.c
+++ synce/src/synce_file.c
@@ -34,6 +34,7 @@
 #include <glib.h>
 
 #include <opensync/opensync.h>
+#include <opensync/file.h>
 
 #include <rra/appointment.h>
 #include <rra/contact.h>
@@ -45,65 +46,53 @@
 #include <synce.h>
 #include "synce_plugin.h"
 
-/*
- * Working with the "file" format requires use of the first couple of fields
- * in the structure below.
- */
-typedef struct	MyCeFileInfo {
-	/* Start of piece copied from fs_fileinfo - don't change */
-	struct stat	filestats;
-	char		*data;		/* Must contain the actual content of the file */
-	int		size;
-	/* End of piece copied from fs_fileinfo - don't change */
-	DWORD		attrib;
-	FILETIME	time_create, time_write;
-	WCHAR		wfn[MAX_PATH];
-} MyCeFileInfo;
+static time_t CeTimeToUnixTime(FILETIME t)
+{
+#ifdef	NEW_TIME
+	TIME_FIELDS	tf;
+	time_fields_from_filetime(&t, &tf);
+	return 0;
+#else
+	time_t seconds;
+	seconds = filetime_to_unix_time(&p->time_create);
+	return seconds;
+#endif
+}
 
 /*
  * A hash appears to be a string.
  * The file-sync plugin allocates new memory in this occasion so I guess that's the right
  * thing to do.
  */
-static char *FileHash(struct MyCeFileInfo *p)
+static char *FileHash(fileFormat *p)
 {
-	char	path[MAX_PATH], *x;
-	char	write_time_string[50] = {0},
-		create_time_string[50] = {0};
+	char		ts[50] = {0};
+	struct tm	*tmp;
 #ifndef	NEW_TIME
 	time_t seconds;
 	struct tm	*time_struct = NULL;
 #else
-	TIME_FIELDS	write_time_fields, create_time_fields;
+	TIME_FIELDS	write_time_fields;
 #endif
 
 	if (!p)
 		return "";
-	x = wstr_to_current(p->wfn);
-	strcpy(path, x);
-	free(x);
 
 #ifndef	NEW_TIME
-	seconds = filetime_to_unix_time(&p->time_create);
+	seconds = filetime_to_unix_time(&p->last_mod);
 	time_struct = localtime(&seconds);
-	strftime(create_time_string, sizeof(create_time_string), "%c", time_struct);
-	seconds = filetime_to_unix_time(&p->time_write);
-	time_struct = localtime(&seconds);
-	strftime(write_time_string, sizeof(write_time_string), "%c", time_struct);
+	strftime(ts, sizeof(ts), "%c", time_struct);
 #else
-	time_fields_from_filetime(&p->time_create, &create_time_fields);
-	time_fields_from_filetime(&p->time_write, &write_time_fields);
-	sprintf(create_time_string, "%04i-%02i-%02i %02i:%02i:%02i",
-		create_time_fields.Year, create_time_fields.Month, create_time_fields.Day,
-		create_time_fields.Hour, create_time_fields.Minute, create_time_fields.Second); 
-	sprintf(write_time_string, "%04i-%02i-%02i %02i:%02i:%02i",
+	tmp = localtime(&p->last_mod);
+	strftime(ts, sizeof(ts), "%c", tmp);
+	sprintf(ts, "%04i-%02i-%02i %02i:%02i:%02i",
 		write_time_fields.Year, write_time_fields.Month, write_time_fields.Day,
 		write_time_fields.Hour, write_time_fields.Minute, write_time_fields.Second); 
 #endif
-	return g_strdup_printf("%ld %s %s %s",
-			(long int)p->size, write_time_string, create_time_string, path);
+	return g_strdup_printf("%ld %s", (long int)p->size, ts);
 }
 
+#if 0
 static void my_wstrcpy(WCHAR *dst, WCHAR *src, int ml)
 {
 	int	i;
@@ -113,9 +102,11 @@ static void my_wstrcpy(WCHAR *dst, WCHAR *src, int ml)
 	if (i != ml)
 		dst[i] = 0;
 }
+#endif
 
+#if 0
 /* Read the file through SynCE */
-static osync_bool FileReadFileIntoStruct(HANDLE h, MyCeFileInfo *mip)
+static osync_bool FileReadFileIntoStruct(HANDLE h, fileFormat *mip)
 {
 	char	*buffer;
 	size_t	sz, rsz;
@@ -123,44 +114,29 @@ static osync_bool FileReadFileIntoStruct(HANDLE h, MyCeFileInfo *mip)
 	sz = mip->size;
 	buffer = malloc(sz);
 
-	fprintf(stderr, "Yow %s %d\n", wstr_to_current(mip->wfn), mip->size);
 	if (!CeReadFile(h, buffer, sz, &rsz, NULL)) {
 		/* Error */
 		fprintf(stderr, "CeReadFile failed\n");
 		return FALSE;
 	}
-	fprintf(stderr, "  ->   %d\n", rsz);
 	mip->data = buffer;
 	return TRUE;
-
-#if 0
-	/* A test - write all the files you find locally */
-	{
-		int	fd;
-		static int	cnt = 0;
-		char	p[MAX_PATH];
-
-		sprintf(p, "log/file.%03d", cnt++);
-		fd = open(p, O_CREAT|O_TRUNC|O_WRONLY, 0666);
-		if (fd) {
-			write(fd, buffer, rsz);
-			close(fd);
-		}
-	}
-#endif
 }
+#endif
 
+/*
+ * In this pass of set_data(), do not pass real file contents, but only pass the file name
+ * to the opensync engine in the fileFormat structure's data field.
+ */
 osync_bool FilesReportFileChange(OSyncContext *ctx, const char *dir, CE_FIND_DATA *entry)
 {
 	synce_plugin_environment	*env;
 	OSyncChange			*change;
 	char				path[MAX_PATH], *hash;
-	struct MyCeFileInfo		mi;
 	WCHAR				*wfn;
 	HANDLE				h;
-
-	fprintf(stderr, "FilesReportFileChange(%s\\%s)\n", dir, wstr_to_current(entry->cFileName));
-
+	fileFormat			ff;
+	time_t				t1, t2;
 
 	sprintf(path, "%s\\%s", dir, wstr_to_current(entry->cFileName));
 	wfn = wstr_from_current(path);
@@ -174,27 +150,36 @@ osync_bool FilesReportFileChange(OSyncContext *ctx, const char *dir, CE_FIND_DAT
 		osync_change_set_objformat_string(change, "file");	/* Copied from file-sync */
 
 		/* Assemble a structure with data about this file */
-		mi.attrib = CeGetFileAttributes(wfn);
 		h = CeCreateFile(wfn, GENERIC_READ, 0, NULL,
 				OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-		/* Include create and write times, but not access time. */
-		mi.time_create = entry->ftCreationTime;
-		mi.time_write = entry->ftLastWriteTime;
-		mi.size = CeGetFileSize(h, NULL);
-		my_wstrcpy(mi.wfn, wfn, MAX_PATH);
+		ff.userid = 0;
+		ff.groupid = 0;
 
-		if (! FileReadFileIntoStruct(h, &mi)) {
+		/* Select the latest time */
+		t1 = CeTimeToUnixTime(entry->ftLastWriteTime);
+		t2 = CeTimeToUnixTime(entry->ftCreationTime);
+		ff.last_mod = (t1 < t2) ? t2 : t1;
+
+		ff.size = CeGetFileSize(h, NULL);
+		ff.mode = 0777;			/* Fake */
+
+#if 0
+		if (! FileReadFileIntoStruct(h, &ff)) {
 			osync_context_report_error(ctx, 1,
 					"Problem reading the file [%s]\n",
 					path);
 			return FALSE;
 		}
+#endif
 		CeCloseHandle(h);
 
 		/* Set the hash */
-		hash = FileHash(&mi);
-		osync_change_set_hash(change, FileHash(&mi));
-		osync_change_set_data(change, (char *)&mi, sizeof(mi), FALSE);
+		hash = FileHash(&ff);
+
+		ff.data = strdup(path);
+
+		osync_change_set_hash(change, hash);
+		osync_change_set_data(change, (char *)&ff, sizeof(ff), FALSE);
 
 		if (osync_hashtable_detect_change(env->hashtable, change)) {
 #if 0
@@ -204,13 +189,14 @@ osync_bool FilesReportFileChange(OSyncContext *ctx, const char *dir, CE_FIND_DAT
 			osync_hashtable_update_hash(env->hashtable, change);
 		}
 		g_free(hash);
-
+#if 0
 		/*
 		 * Pass the structure to OpenSync
 		 * The final parameter is FALSE to indicate a get_data() call will fetch
 		 * the file contents if necessary later on.
 		 */
-		osync_change_set_data(change, (char *)&mi, sizeof(struct MyCeFileInfo), FALSE);
+		osync_change_set_data(change, (char *)&ff, sizeof(fileFormat), FALSE);
+#endif
 	}
 
 	wstr_free_string(wfn);
@@ -227,7 +213,6 @@ osync_bool FilesFindAllFromDirectory(OSyncContext *ctx, const char *dir)
 
 	if (! dir)
 		return TRUE;
-	fprintf(stderr, "FilesFindAllFromDirectory(%s)\n", dir);
 	sprintf(path, "%s\\*", dir);
 
 	wd = wstr_from_current(path);
@@ -243,7 +228,6 @@ osync_bool FilesFindAllFromDirectory(OSyncContext *ctx, const char *dir)
 				return FALSE;
 			}
 
-			fprintf(stderr, "-> (%s)\n", wstr_to_current(entry->cFileName));
 
 			/* Recursive call for directories */
 			if (entry->dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
@@ -424,45 +408,105 @@ extern void file_finalize(void *data)
 extern void file_get_data(OSyncContext *ctx, OSyncChange *change)
 {
 	synce_plugin_environment	*env;
-	struct MyCeFileInfo	*mip;
-	HANDLE			h;
-	char			*buffer;
-	size_t			sz, rsz;
-
+	fileFormat			*ff;
+	HANDLE				h;
+	size_t				rsz;
+	WCHAR				*wfn;
+	int				r;
 
 	osync_debug("SynCE-File", 4, "start : %s", __func__);
 	env = (synce_plugin_environment *)osync_context_get_plugin_data(ctx);
-	mip = (struct MyCeFileInfo *)osync_change_get_data(change);
+	ff = (fileFormat *)osync_change_get_data(change);
+
+	/* There appears to be a trick being used here.
+	 * First report some stuff, use the data field to pass random data (e.g. the file
+	 * name) to ourselved.
+	 * Then, in the get_data() call, read that data and replace it with the file content.
+	 */
+	wfn = wstr_from_current(ff->data);
 
 	/* Read the file through SynCE */
-	sz = 4096;
-	buffer = malloc(sz);
+	h = CeCreateFile(wfn, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
 
-	h = CeCreateFile(mip->wfn, GENERIC_READ, 0, NULL,
-			OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
-	/*
-	 * Should do this in a loop, as CeReadFile() will return success
-	 * with 0 bytes read when we're done.
-	 */
-	rsz = 1;
-	while (rsz) {
-		if (!CeReadFile(h, buffer, sz, &rsz, NULL)) {
-			/* Error */
-			osync_context_report_error(ctx, 1, "Error from CeReadFile");
-			CeCloseHandle(h);
-			return;
-		}
-		/* Send its contents */
-		if (ctx)
-			osync_change_set_data(change,
-				buffer, rsz,
-				(rsz == 0) ? TRUE : FALSE);	/* TRUE if complete */
+	free(ff->data);
+	ff->data = malloc(ff->size);
+
+	r = CeReadFile(h, ff->data, ff->size, &rsz, NULL);
+	if (r == 0) {
+		/* Error */
+		DWORD	e = CeGetLastError();
+		char	*s = synce_strerror(e);
+		osync_context_report_error(ctx, 1, "Error from CeReadFile (%s)", s);
+		CeCloseHandle(h);
+		return;
 	}
-	fprintf(stderr, "CeReadFile ok\n");
+	/* Send its contents */
+	if (ctx)
+		osync_change_set_data(change, (char *)ff, sizeof(ff), TRUE);
 	CeCloseHandle(h);
 
+	wstr_free_string(wfn);
 	osync_context_report_success(ctx);
 	osync_debug("SynCE-File", 4, "end : %s", __func__);
+}
+
+#if 0
+extern void file_read(OSyncContext *ctx, OSyncChange *change)
+{
+	fileFormat	*mip;
+
+	osync_debug("SynCE-SYNC", 4, "start: %s", __func__);
+	mip = (fileFormat *)osync_context_get_plugin_data(ctx);
+
+	osync_debug("SynCE-SYNC", 4, "end: %s", __func__);
+}
+
+extern osync_bool file_access(OSyncContext *ctx, OSyncChange *change)
+{
+	fileFormat	*mip;
+
+	osync_debug("SynCE-SYNC", 4, "start: %s", __func__);
+	mip = (fileFormat *)osync_context_get_plugin_data(ctx);
+
+	osync_debug("SynCE File", 4, "file_access(%s)\n", mip->wfn);
+	osync_context_report_success(ctx);
+	osync_debug("SynCE-SYNC", 4, "end: %s", __func__);
+	return TRUE;
+}
+#endif
+
+/*
+ * This function is called by the configuration GUI,
+ * it gives it a list of interesting directories on the PDA,
+ * which the user might like to choose from to synchronize.
+ *
+ * Do not make this function static, as the confuration GUI needs to be able to locate
+ * it using dlsym(); also don't change its name or signature without making the
+ * corresponding change in the configuration program.
+ */
+GList * synce_list_directories(synce_plugin_environment *env, const char *basedir, OSyncError **error)
+{
+	GList	*ret = NULL;
+	WCHAR	path[MAX_PATH];
+	char	*s = NULL;
+	HRESULT hr;
+	int	r;
+
+	hr = CeRapiInit();
+
+	int	i;
+
+	for (i=0; i<255; i++) {
+		r = CeGetSpecialFolderPath(i, MAX_PATH, path);
+		if (r > 0) {
+			s = wstr_to_current(path);
+			ret = g_list_append(ret, s);
+			free(s);
+		}
+	}
+
+	CeRapiUninit();
+	return ret;
 }
 
 #ifdef	TEST_FILE
@@ -483,6 +527,8 @@ int main(int argc, char** argv)
 	FilesFindAllFromDirectory(NULL, "\\Storage Card");
 	FilesFindAllFromDirectory(NULL, "\\My Documents\\foto");
 
+	(void) synce_list_directories(NULL, NULL, NULL);
+
 exit:
 	if (path)
 		free(path);
@@ -493,26 +539,3 @@ exit:
 	return 0;
 }
 #endif
-
-extern void file_read(OSyncContext *ctx, OSyncChange *change)
-{
-	MyCeFileInfo	*mip;
-
-	osync_debug("SynCE-SYNC", 4, "start: %s", __func__);
-	mip = (MyCeFileInfo *)osync_context_get_plugin_data(ctx);
-
-	osync_debug("SynCE-SYNC", 4, "end: %s", __func__);
-}
-
-extern osync_bool file_access(OSyncContext *ctx, OSyncChange *change)
-{
-	MyCeFileInfo	*mip;
-
-	osync_debug("SynCE-SYNC", 4, "start: %s", __func__);
-	mip = (MyCeFileInfo *)osync_context_get_plugin_data(ctx);
-
-	osync_debug("SynCE File", 4, "file_access(%s)\n", mip->wfn);
-	osync_context_report_success(ctx);
-	osync_debug("SynCE-SYNC", 4, "end: %s", __func__);
-	return TRUE;
-}
