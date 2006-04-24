c0d554de3cce2443b1df94ae129e8e043835f2a6
abauer <abauer>
2006-04-24 14:03:44 +0000

    Added the first version of the sunbird plugin

---
+++ sunbird/src/webdav.c
@@ -0,0 +1,151 @@
+#include "webdav.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <neon/ne_socket.h>
+#include <neon/ne_session.h>
+#include <neon/ne_request.h>
+#include <neon/ne_auth.h>
+#include <neon/ne_basic.h>
+
+static char auth_username[100];
+static char auth_password[100];
+static int neon_initialized = 0;
+
+static int webdav_server_auth(void* userdata, const char *realm, int attempts, char *username, char *password)
+{
+    strcpy(username, auth_username);
+    strcpy(password, auth_password);
+    return attempts;
+}
+
+static int init_neon()
+{
+    if (!neon_initialized)
+        neon_initialized = (ne_sock_init() == 0);
+    return neon_initialized;
+}
+
+static int webdav_spliturl(char* url, char* server, char* path)
+{
+    char *p1, *p2;
+    
+    p1 = strstr(url, "://");
+    if (!p1)
+        return 0;
+    p1 += 3;
+        
+    p2 = strstr(p1, "/");
+    if (!p2)
+        return 0;
+    
+    strcpy(server, p1);
+    server[p2-p1] = 0;
+    strcpy(path, p2);
+    
+    return 1;
+}
+
+int webdav_download(char* filename, char* url, char* username, char* password)
+{
+    char server[256], path[256];
+    int result;
+    ne_session* sess;
+    FILE* f;
+    
+    if (strlen(url) > 255 || strlen(username) > 99 || strlen(password) > 99)
+        return WEBDAV_ERROR_INVALID_PARAMETER;
+
+    if (!webdav_spliturl(url, server, path))
+        return WEBDAV_ERROR_INVALID_PARAMETER;
+    
+    f = fopen(filename, "w");
+    if (!f)
+        return WEBDAV_ERROR_FILE_NOT_FOUND;
+
+    strcpy(auth_username, username);
+    strcpy(auth_password, password);
+    
+    if (!init_neon())
+        return WEBDAV_ERROR_INIT;
+        
+    sess = ne_session_create("http", server, 80);
+    if (!sess)
+        return WEBDAV_ERROR_CONNECT;
+
+    ne_set_server_auth(sess, webdav_server_auth, 0);
+    result = ne_get(sess, path, fileno(f)) ? WEBDAV_ERROR_RESSOURCE : WEBDAV_SUCCESS;
+    fclose(f);
+    ne_session_destroy(sess);
+
+    return result;
+}
+
+int webdav_upload(char* filename, char* url, char* username, char* password)
+{
+    char server[256], path[256], *buf;
+    int result;
+    int filesize;
+    ne_session* sess;
+    ne_request* req;
+    FILE* f;
+    
+    if (strlen(url) > 255 || strlen(username) > 99 || strlen(password) > 99)
+        return WEBDAV_ERROR_INVALID_PARAMETER;
+
+    if (!webdav_spliturl(url, server, path))
+        return WEBDAV_ERROR_INVALID_PARAMETER;
+    
+    f = fopen(filename, "r");
+    if (!f)
+        return WEBDAV_ERROR_FILE_NOT_FOUND;
+    fseek(f, 0, SEEK_END);
+    filesize = ftell(f);
+    rewind(f);
+    buf = (char*)malloc(filesize);
+    if (!buf)
+    {
+        fclose(f);
+        return WEBDAV_ERROR_OUT_OF_MEMORY;
+    }
+    fread(buf, 1, filesize, f);
+    fclose(f);
+
+    strcpy(auth_username, username);
+    strcpy(auth_password, password);
+    
+    if (!init_neon())
+        return WEBDAV_ERROR_INIT;
+        
+    sess = ne_session_create("http", server, 80);
+    if (!sess)
+    {
+        free(buf);
+        return WEBDAV_ERROR_CONNECT;
+    }
+
+    ne_set_server_auth(sess, webdav_server_auth, 0);
+
+    req = ne_request_create(sess, "PUT", path);
+    ne_set_request_body_buffer(req, buf, filesize);
+    if (ne_request_dispatch(req) != 0)
+    {
+        ne_request_destroy(req);
+        ne_session_destroy(sess);
+        free(buf);
+        return WEBDAV_ERROR_RESSOURCE;
+    }
+
+    result = ne_get_status(req)->code;
+    
+    ne_request_destroy(req);
+    ne_session_destroy(sess);
+    free(buf);
+    
+    if (result < 200 || result > 299)
+        return WEBDAV_ERROR_RESSOURCE;
+    else
+        return WEBDAV_SUCCESS;
+}
