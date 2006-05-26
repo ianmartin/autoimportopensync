49900acb68eead31f71d19ccc6c1adddf2d5c7f9
dgollub <dgollub>
2006-05-26 20:39:34 +0000

    fixed gcc compiler warning added: osync_hashtable_get_changetype() is required by python wrapper

--- opensync/opensync_hashtable.h
+++ opensync/opensync_hashtable.h
@@ -10,3 +10,4 @@ int osync_hashtable_num_entries(OSyncHashTable *table);
 osync_bool osync_hashtable_nth_entry(OSyncHashTable *table, int i, char **uid, char **hash);
 void osync_hashtable_set_slow_sync(OSyncHashTable *table, const char *objtype);
 void osync_hashtable_report(OSyncHashTable *table, const char *uid);
+OSyncChangeType osync_hashtable_get_changetype(OSyncHashTable *table, const char *uid, const char *hash);
