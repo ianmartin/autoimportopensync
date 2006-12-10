1da4cab15fed8fcf1c73fa50ec17e43b19c9d9ec
dgollub <dgollub>
2006-12-10 21:42:30 +0000

    Added database module.
    If you are writing modules which store data
    in a database, use this module.
    
    Splitted all sqlite3 function calls.
    (If we should decide to switch to another DBMS,
     we can start here...)

---
+++ opensync/db/opensync_db.c
@@ -0,0 +1,377 @@
+/*
+ * libopensync - A synchronization framework
+ * Copyright (C) 2006  Daniel Gollub <dgollub@suse.de>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 
+ */
+
+#include "opensync.h"
+#include "opensync_internals.h"
+#include "opensync_db_internals.h"
+#include "opensync_db_internals.h"
+
+/*
+static void _osync_db_trace(void *data, const char *query)
+{
+	osync_trace(TRACE_INTERNAL, "osync_db query executed: %s", query);
+}
+*/
+
+OSyncDB *osync_db_new(OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p)", __func__, error);
+
+	OSyncDB *db = osync_try_malloc0(sizeof(OSyncDB), error);
+	if (!db) {
+		osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));
+		return NULL;
+	}
+
+	osync_trace(TRACE_EXIT, "%s: %p", __func__, db);
+	return db;
+}
+
+/**
+ * @brief Open a database
+ * 
+ * @param db Pointer to database struct
+ * @param dbfile Full file path to database file
+ * @param error Pointer to a error struct 
+ * @return If database opened successfully then TRUE else FALSE
+ */
+osync_bool osync_db_open(OSyncDB *db, const char *dbfile, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, dbfile, error);
+
+	osync_assert(dbfile);
+
+	int rc = sqlite3_open(dbfile, &(db->sqlite3db));
+	if (rc) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Cannot open database: %s", sqlite3_errmsg(db->sqlite3db));
+		osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, sqlite3_errmsg(db->sqlite3db));
+		return FALSE;
+	}
+
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+}
+
+/**
+ * @brief Close a sqlite3 database file
+ * 
+ * @param db Pointer to database struct
+ * @param error Pointer to a error struct 
+ * @return If database closed successfully then TRUE else FALSE
+ */
+osync_bool osync_db_close(OSyncDB *db, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p)", __func__, db, error);
+	
+	int rc = sqlite3_close(db->sqlite3db);
+	if (rc) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Cannot close database: %s", sqlite3_errmsg(db->sqlite3db));
+		osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, sqlite3_errmsg(db->sqlite3db));
+		return FALSE;
+	}
+
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+}
+
+/**
+ * @brief Counts result of a database query 
+ * 
+ * @param db Pointer to database struct
+ * @param query SQL database query 
+ * @param error Pointer to a error struct 
+ * @return Returns number of query result or -1 on error 
+ */
+int osync_db_count(OSyncDB *db, const char *query, OSyncError **error)
+{
+	int num;
+
+	char **result = NULL;
+
+	sqlite3_get_table(db->sqlite3db, query, &result, &num, NULL, NULL);
+	sqlite3_free_table(result);
+	
+	return num;
+}
+
+/**
+ * @brief Executes a simple SQL query 
+ * 
+ * @param db Pointer to database struct
+ * @param query SQL database query 
+ * @param error Pointer to a error struct 
+ * @return Returns TRUE on success otherwise FALSE 
+ */
+osync_bool osync_db_query(OSyncDB *db, const char *query, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, query, error);
+	char *errmsg = NULL;
+
+	if (sqlite3_exec(db->sqlite3db, query, NULL, NULL, &errmsg) != SQLITE_OK) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to execute simple query: %s", errmsg);
+		osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, errmsg);
+		g_free(errmsg);
+		return FALSE;
+	}
+
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+}
+
+/* TODO */
+GList *osync_db_query_table(OSyncDB *db, const char *query, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, query, error);
+	
+	GList *table = NULL;
+	int i, j, column_count = 0;
+	int numrows = 0, numcolumns = 0;
+	char **result = NULL;
+
+	sqlite3_get_table(db->sqlite3db, query, &result, &numrows, &numcolumns, NULL);
+	
+	/* First row contains only names of columns. So we skip this!
+	   We start reading the array with a offset (number of columns) */
+	column_count = numcolumns;
+
+	for (j=0; j < numrows; j++) {
+		GList *row = NULL;
+		for (i=0; i < numcolumns; i++)
+			row = g_list_append(row, g_strdup(result[column_count++]));
+
+		table = g_list_append(table, row);
+	}
+
+	sqlite3_free_table(result);
+
+	osync_trace(TRACE_EXIT, "%s: %p", __func__, table);
+	return table; 
+}
+
+void osync_db_free_list(GList *list) {
+	osync_trace(TRACE_ENTRY, "%s(%p)", __func__, list);
+
+	GList *row;
+	for (row = list; row; row = row->next)
+		g_list_foreach((GList *) row->data, (GFunc) g_free, NULL);
+
+	g_list_free(list);
+
+	osync_trace(TRACE_EXIT, "%s", __func__);
+}
+
+/**
+ * @brief Execute a SQL query for a single string (char *). Do only use this
+ *        function when you expect a single return. The function will return
+ *        NULL if there are more then one results! 
+ *
+ * @param db Pointer to database struct
+ * @param query SQL database query 
+ * @param error Pointer to a error struct 
+ * @return Returns result string from database. NULL on more then one result or on error (the caller is responsible for freeing)
+ */
+char *osync_db_query_single_string(OSyncDB *db, const char *query, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, query, error);
+	
+	char *result = NULL;
+	sqlite3_stmt *ppStmt = NULL;
+
+	sqlite3_prepare(db->sqlite3db, query, -1, &ppStmt, NULL);
+
+	if (sqlite3_step(ppStmt) != SQLITE_ROW) {
+		sqlite3_finalize(ppStmt);
+		osync_trace(TRACE_EXIT, "%s: no result of query", __func__);
+		return NULL;
+	}
+	
+	result = g_strdup((const char *)sqlite3_column_text(ppStmt, 0));
+
+	if (sqlite3_step(ppStmt) == SQLITE_ROW) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Returned more than one result! This function only handle a single string!");
+		goto error;
+	}
+	
+	sqlite3_finalize(ppStmt);
+	
+	osync_trace(TRACE_EXIT, "%s: %s", __func__, result);
+	return result; 
+	
+error:
+	g_free(result);
+	sqlite3_finalize(ppStmt);
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));
+	return NULL;
+}
+
+/**
+ * @brief Execute a SQL query for a single integer. Do only use this
+ *        function when you expect a single return. The function will return
+ *        NULL if there are more then one results! 
+ *
+ * @param db Pointer to database struct
+ * @param query SQL database query 
+ * @param error Pointer to a error struct 
+ * @return Returns result integer from database.  On more then one result or on error -1 
+ */
+int osync_db_query_single_int(OSyncDB *db, const char *query, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, query, error);
+	
+	int result = 0;
+	sqlite3_stmt *ppStmt = NULL;
+
+	sqlite3_prepare(db->sqlite3db, query, -1, &ppStmt, NULL);
+
+	if (sqlite3_step(ppStmt) != SQLITE_ROW) {
+		sqlite3_finalize(ppStmt);
+		osync_trace(TRACE_EXIT, "%s: no result of query", __func__);
+		return -1;
+	}
+	
+	result = sqlite3_column_int(ppStmt, 0);
+
+	if (sqlite3_step(ppStmt) == SQLITE_ROW) {
+		osync_error_set(error, OSYNC_ERROR_GENERIC, "Returned more than one result! This function only handle a single integer!");
+		goto error;
+	}
+	
+	sqlite3_finalize(ppStmt);
+	
+	osync_trace(TRACE_EXIT, "%s: %s", __func__, result);
+	return result; 
+	
+error:
+	sqlite3_finalize(ppStmt);
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));
+	return -1;
+}
+
+/**
+ * @brief Full reset of a sqlite3 table. 
+ *
+ * @param db Pointer to database struct
+ * @param tablename Name of table which gets reseted
+ * @param error Pointer to a error struct 
+ * @return TRUE on success otherwise FALSE
+ */
+osync_bool osync_db_reset(OSyncDB *db, const char *tablename, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, tablename, error);
+	osync_assert(db);
+	osync_assert(tablename);
+	
+	char *query = g_strdup_printf("DELETE FROM %s", tablename);
+
+	if (!osync_db_query(db, query, error))
+		goto error;
+
+	g_free(query);
+	
+	osync_trace(TRACE_EXIT, "%s", __func__);
+	return TRUE;
+error:
+	g_free(query);
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error));	
+	return FALSE;
+}
+
+osync_bool osync_db_reset_full(OSyncDB *db, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %p)", __func__, db, error);
+
+	sqlite3_stmt *ppStmt = NULL;
+	char *query = g_strdup("SELECT name FROM (SELECT * FROM sqlite_master) WHERE type='table'");
+
+	sqlite3_prepare(db->sqlite3db, query, -1, &ppStmt, NULL);
+
+	while (sqlite3_step(ppStmt) == SQLITE_ROW) {
+		const char *table = (const char *) sqlite3_column_text(ppStmt, 0);
+		if (!osync_db_reset(db, table, error))
+			goto error;
+	}
+
+	sqlite3_finalize(ppStmt);
+
+	osync_trace(TRACE_EXIT, "%s: TRUE", __func__);
+	return TRUE;
+
+error:
+	sqlite3_finalize(ppStmt);
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error)); 
+	return FALSE;
+}
+
+osync_bool osync_db_reset_full_by_path(const char *path, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%s, %p)", __func__, path, error);
+
+	OSyncDB *db = NULL;
+	if (!osync_db_open(db, path, error))
+		goto error;
+
+	if (!osync_db_reset_full(db, error))
+		goto error;
+
+	osync_trace(TRACE_EXIT, "%s: TRUE", __func__);
+	return TRUE;
+error:
+	osync_trace(TRACE_EXIT_ERROR, "%s: %s", __func__, osync_error_print(error)); 
+	return FALSE;
+
+}
+
+
+/**
+ * @brief Checks if requted table exists in database
+ *
+ * @param db Pointer to database struct
+ * @param tablename Name of table which gets reseted
+ * @param error Pointer to a error struct 
+ * @return TRUE on success otherwise FALSE
+ */
+osync_bool osync_db_exists(OSyncDB *db, const char *tablename, OSyncError **error)
+{
+	osync_trace(TRACE_ENTRY, "%s(%p, %s, %p)", __func__, db, tablename, error);
+	osync_assert(db);
+	osync_assert(tablename);
+	
+	sqlite3_stmt *ppStmt = NULL;
+
+	char *query = g_strdup_printf("SELECT name FROM (SELECT * FROM sqlite_master UNION ALL SELECT * FROM sqlite_temp_master) WHERE type='table' AND name='%s'",
+			tablename);
+
+	sqlite3_prepare(db->sqlite3db, query, -1, &ppStmt, NULL);
+
+	if (sqlite3_step(ppStmt) != SQLITE_ROW) {
+		sqlite3_finalize(ppStmt);
+		g_free(query);
+
+		osync_trace(TRACE_EXIT, "%s: table \"%s\" doesn't exist. FALSE", __func__, tablename);
+		return FALSE;
+	}
+
+	sqlite3_finalize(ppStmt);
+	g_free(query);
+	
+	osync_trace(TRACE_EXIT, "%s: table \"%s\" exists. TRUE", __func__, tablename);
+	return TRUE;
+}
+
