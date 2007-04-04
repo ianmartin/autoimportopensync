c0af04400ab5eaf079ae1ddce82ce434aa9370c7
jerryyu <jerryyu>
2007-04-04 09:51:11 +0000

    Roll back to r1867 because of '^M' in recent two patches.

--- formats/todo.c
+++ formats/todo.c
@@ -1,7 +1,6 @@
 /*
  * todo - A plugin for todo objects for the opensync framework
  * Copyright (C) 2004-2005  Armin Bauer <armin.bauer@opensync.org>
- * Copyright (C) 2007  Jerry Yu <jijun.yu@sun.com>
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,127 +19,57 @@
  */
  
 #include "opensync/opensync.h"
-#include "opensync/opensync_internals.h"
-#include "opensync/opensync-support.h"
-#include "opensync/opensync-serializer.h"
-#include "opensync/opensync-format.h"
 #include <glib.h>
 #include <string.h>
-/** @defgroup todo_vtodo todo/vtodo data format */
 
-static OSyncConvCmpResult compare_vtodo(const char *leftdata, unsigned int leftsize, const char *rightdata, unsigned int rightsize)
+static OSyncConvCmpResult compare_vtodo(OSyncChange *leftchange, OSyncChange *rightchange)
 {
-	/* Consider empty block equal NULL pointers */ 
-	if (!leftsize) leftdata = NULL;
-	if (!rightsize) rightdata = NULL;
-	
-	if (!leftdata && !rightdata)
-		return OSYNC_CONV_DATA_SAME;
-
-	if (leftdata && rightdata && (leftsize == rightsize)) {
-		if (!memcmp(leftdata, rightdata, leftsize))
-			return OSYNC_CONV_DATA_SAME;
-		else
-			return OSYNC_CONV_DATA_MISMATCH;
-	}
-
-	return OSYNC_CONV_DATA_MISMATCH;
+	return CONV_DATA_MISMATCH;
 }
 
-static osync_bool detect_plain_as_vtodo10(const char *data, int size)
+static osync_bool detect_plain_as_vtodo10(OSyncFormatEnv *env, const char *data, int size)
 {
-	osync_trace(TRACE_INTERNAL, "start: %s", __func__);
+	osync_debug("VCAL", 3, "start: %s", __func__);
 
 	return g_pattern_match_simple("*BEGIN:VCALENDAR*VERSION:1.0*BEGIN:VTODO*", data);
 }
 
-static osync_bool detect_plain_as_vtodo20(const char *data, int size)
+static osync_bool detect_plain_as_vtodo20(OSyncFormatEnv *env, const char *data, int size)
 {
-	osync_trace(TRACE_INTERNAL, "start: %s", __func__);
+	osync_debug("VCAL", 3, "start: %s", __func__);
 
 	return g_pattern_match_simple("*BEGIN:VCALENDAR*VERSION:2.0*BEGIN:VTODO*", data);
 }
 
-static void create_vtodo10(char **data, unsigned int *size)
+static void create_todo10(OSyncChange *change)
 {
-	*data = g_strdup_printf("BEGIN:VCALENDAR\r\nPRODID:-//OpenSync//NONSGML OpenSync TestGenerator//EN\r\nVERSION:1.0\r\nBEGIN:VTODO\r\nSUMMARY:%s\r\nEND:VTODO\r\nEND:VCALENDAR", osync_rand_str(20));
-	*size = strlen(*data);	
+	char *vtodo = g_strdup_printf("BEGIN:VCALENDAR\r\nPRODID:-//OpenSync//NONSGML OpenSync TestGenerator//EN\r\nVERSION:1.0\r\nBEGIN:VTODO\r\nSUMMARY:%s\r\nEND:VTODO\r\nEND:VCALENDAR", osync_rand_str(20));
+	
+	osync_change_set_data(change, vtodo, strlen(vtodo) + 1, TRUE);
+	if (!osync_change_get_uid(change))
+		osync_change_set_uid(change, osync_rand_str(6));
 }
 
-static void create_vtodo20(char **data, unsigned int *size)
+static void create_todo20(OSyncChange *change)
 {
-	*data = g_strdup_printf("BEGIN:VCALENDAR\r\nPRODID:-//OpenSync//NONSGML OpenSync TestGenerator//EN\r\nVERSION:2.0\r\nBEGIN:VTODO\r\nSUMMARY:%s\r\nEND:VTODO\r\nEND:VCALENDAR", osync_rand_str(20));
-	*size = strlen(*data);	
+	char *vtodo = g_strdup_printf("BEGIN:VCALENDAR\r\nPRODID:-//OpenSync//NONSGML OpenSync TestGenerator//EN\r\nVERSION:2.0\r\nBEGIN:VTODO\r\nSUMMARY:%s\r\nEND:VTODO\r\nEND:VCALENDAR", osync_rand_str(20));
+	
+	osync_change_set_data(change, vtodo, strlen(vtodo) + 1, TRUE);
+	if (!osync_change_get_uid(change))
+		osync_change_set_uid(change, osync_rand_str(6));
 }
 
-static void destroy_vtodo(char *input, size_t inpsize)
+void get_info(OSyncEnv *env)
 {
-	g_free(input);
-}
-
-osync_bool get_format_info(OSyncFormatEnv *env, OSyncError **error)
-{
-	OSyncObjFormat *format = osync_objformat_new("vtodo10", "todo", error);
-	if (!format)
-		return FALSE;
+	osync_env_register_objtype(env, "todo");
 	
-	osync_objformat_set_compare_func(format, compare_vtodo);
-	osync_objformat_set_create_func(format, create_vtodo10);
-	osync_objformat_set_destroy_func(format, destroy_vtodo);
+	osync_env_register_objformat(env, "todo", "vtodo10");
+	osync_env_format_set_compare_func(env, "vtodo10", compare_vtodo);
+	osync_env_format_set_create_func(env, "vtodo10", create_todo10);
+	osync_env_register_detector(env, "plain", "vtodo10", detect_plain_as_vtodo10);
 	
-	osync_format_env_register_objformat(env, format);
-	osync_objformat_unref(format);
-
-	format = osync_objformat_new("vtodo20", "todo", error);
-	if (!format)
-		return FALSE;
-	
-	osync_objformat_set_compare_func(format, compare_vtodo);
-	osync_objformat_set_create_func(format, create_vtodo20);
-	osync_objformat_set_destroy_func(format, destroy_vtodo);
-	
-	osync_format_env_register_objformat(env, format);
-	osync_objformat_unref(format);
-
-	return TRUE;
-}
-
-osync_bool get_conversion_info(OSyncFormatEnv *env, OSyncError **error)
-{
-	OSyncObjFormat *plain = osync_format_env_find_objformat(env, "plain");
-	if (!plain) {
-		osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to find plain format");
-		return FALSE;
-	}
-	
-	OSyncObjFormat *vtodo10 = osync_format_env_find_objformat(env, "vtodo10");
-	if (!vtodo10) {
-		osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to find vtodo10 format");
-		return FALSE;
-	}
-	
-	OSyncObjFormat *vtodo20 = osync_format_env_find_objformat(env, "vtodo20");
-	if (!vtodo20) {
-		osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to find vtodo20 format");
-		return FALSE;
-	}
-	
-	OSyncFormatConverter *conv = osync_converter_new_detector(plain, vtodo10, detect_plain_as_vtodo10, error);
-	if (!conv)
-		return FALSE;
-	osync_format_env_register_converter(env, conv);
-	osync_converter_unref(conv);
-	
-	conv = osync_converter_new_detector(plain, vtodo20, detect_plain_as_vtodo20, error);
-	if (!conv)
-		return FALSE;
-	osync_format_env_register_converter(env, conv);
-	osync_converter_unref(conv);
-
-	return TRUE;
-}
-	
-int get_version (void)
-{
-	return 1;
+	osync_env_register_objformat(env, "todo", "vtodo20");
+	osync_env_format_set_compare_func(env, "vtodo20", compare_vtodo);
+	osync_env_format_set_create_func(env, "vtodo20", create_todo20);
+	osync_env_register_detector(env, "plain", "vtodo20", detect_plain_as_vtodo20);
 }
