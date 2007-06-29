e0b06848233c019ad788ec6427d894cb48a747c4
dgollub <dgollub>
2007-06-29 00:42:11 +0000

    Implemented missing D-Bus methods .. most of them are NOP.

--- MobileStation.py
+++ MobileStation.py
@@ -32,7 +32,7 @@ class SyncMember(dbus.service.Object):
 	self.member = member
 	# FIXME: Copy the PluginName since the member object got lost... 
 	self.plugin_name = member.get_pluginname()		    
-	print self.member.get_pluginname()		    
+	#print self.member.get_pluginname()		    
 
     @dbus.service.method("org.opensync.SyncMember", in_signature='', out_signature='s')
     def GetPluginName(self):
@@ -42,12 +42,12 @@ class SyncMember(dbus.service.Object):
 
 class SyncGroup(dbus.service.Object):
 
+    syncMembers = []
+
     def __init__(self, bus_name, group, group_id):
 	self.group = group
 	self.group_id = group_id
         dbus.service.Object.__init__(self, bus_name, str("/org/opensync/group%i/Manager" % group_id))
-
-    	self.syncMembers = []
 
 	num_member = self.group.num_members
 	i = 0 
@@ -57,12 +57,68 @@ class SyncGroup(dbus.service.Object):
 	    syncMember = SyncMember(bus_name, group_id, member, i)
 	    self.syncMembers.append(syncMember._object_path)
 
+    def EngineStatusCallback(self, status):
+"""	    
+	if opensync.ENGINE_COMMAND_SYNC_DONE == status:
+		self.engine.finalize()
+"""
+
+    @dbus.service.method("org.opensync.SyncGroup", in_signature='', out_signature='')
+    def Sync(self):
+"""
+	self.engine = opensync.Engine(self.group)
+	self.engine.initialize()
+
+	self.engine.set_enginestatus_callback(self.EngineStatusCallback)
+
+	self.engine.synchronize()
+"""
+
 
     @dbus.service.method("org.opensync.SyncGroup", in_signature='', out_signature='as')
     def ListMembers(self):
 	return self.syncMembers	  
 
+
+class MobileStationDevice(dbus.service.Object):
+    
+    isConnected = False
+    name = "Fake Device"
+
+    def __init__(self, bus_name, type, address):
+        dbus.service.Object.__init__(self, bus_name, str("/org/opensync/MobileStation/Device/%s" % address))
+	self.type = type
+	self.address = address
+
+    @dbus.service.method("org.opensync.MobileStation.Device", in_signature='', out_signature='b')
+    def IsConnected(self):
+	return self.isConnected
+
+    @dbus.service.method("org.opensync.MobileStation.Device", in_signature='', out_signature='s')
+    def GetConnectionType(self):
+	return self.type
+
+    @dbus.service.method("org.opensync.MobileStation.Device", in_signature='', out_signature='s')
+    def GetName(self):
+	return self.name
+
+    @dbus.service.method("org.opensync.MobileStation.Device", in_signature='', out_signature='s')
+    def GetAddress(self):
+	return self.address
+
+    @dbus.service.method("org.opensync.MobileStation.Device", in_signature='', out_signature='b')
+    def IsPaired(self):
+	if selft.type != "bluetooth":
+		return True 
+	""" TODO: request bonding status via BlueZ of the default adapter """
+	return False
+
 class MobileStationManager(dbus.service.Object):
+
+    registeredPims = []	
+    syncGroups = []
+    bluetoothDevices = []
+    usbDevices = []
 
     def __init__(self, bus_name, object_path="/org/opensync/MobileStation"):
         dbus.service.Object.__init__(self, bus_name, object_path)
@@ -83,10 +139,15 @@ class MobileStationManager(dbus.service.Object):
 	print "ListDevices " + str(self.fakeDevices)
 	return self.fakeDevices
 
-    @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='ss', out_signature='s')
-    def CreateDevice(self, interface, uid):
- 	print "CreateDevice(interface:" + str(interface) + " uid: " + str(uid) +")"
- 	return "/org/opensync/MobileStation/FakeDevice" 
+    @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='s', out_signature='s')
+    def CreateBluetoothDevice(self, address):
+ 	print "CreateBluetoothDevice(address: " + address + ")"
+ 	return "/org/opensync/MobileStation/Device/" + address 
+
+    @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='s', out_signature='s')
+    def CreateUsbDevice(self, udi):
+ 	print "CreateUsbDevice(udi:" + udi +")"
+ 	return "/org/opensync/MobileStation/Device/0830:0061" 
 
     @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='s', out_signature='')
     def RemoveDevice(self, value):
@@ -98,17 +159,24 @@ class MobileStationManager(dbus.service.Object):
 	return self.syncGroups
 
     @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='s', out_signature='s')
-    def CreateSyncGroup(self, value):
-	print "CreateSyncGroup " + str(value)
-	return "/org/opensync/" + str(value)
+    def CreateSyncGroup(self, name):
+	print "CreateSyncGroup " + str(name)
+	return "/org/opensync/groupX"
+
+    @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='ss', out_signature='s')
+    def SetupSyncDevicePimGroup(self, devicepath, pim):
+	print "SetupSyncDevicePimGroup: " + str(devicepath) + " pim: " + str(pim)
+	return "/org/opensync/groupX"
 
     @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='s', out_signature='')
-    def RemoveSyncGroup (self, value):
-	print "RemoveSyncGroup" + str(value)
+    def RemoveSyncGroup (self, path):
+	print "RemoveSyncGroup" + str(path)
 	return None 
 
-
-		    
+    @dbus.service.method("org.opensync.MobileStation.Manager", in_signature='', out_signature='ss')
+    def ListRegisteredPims (self, path):
+	print "ListRegisteredPims"
+	return self.registeredPims 
 
 if __name__ == "__main__":
 
