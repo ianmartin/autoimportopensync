4fbec8c86dbe444dd3d41ce09f448489a89a48bf
cstender <cstender>
2007-07-03 12:50:08 +0000

    added vtodo support to vconvert

--- vformat/tools/vconvert.c
+++ vformat/tools/vconvert.c
@@ -39,6 +39,8 @@ static void usage (char *name, int ecode)
 	fprintf (stderr, "--to-vcard30\tConvert to vcard 3.0\n");
 	fprintf (stderr, "--to-vevent10\tConvert to vevent 1.0\n");
 	fprintf (stderr, "--to-vevent20\tConvert to vevent 2.0\n");
+	fprintf (stderr, "--to-vtodo10\tConvert to vtodo 1.0\n");
+	fprintf (stderr, "--to-vtodo20\tConvert to vtodo 2.0\n");
 	fprintf (stderr, "--to-xmlformat\tConvert to xmlformat\n");
 	exit (ecode);
 }
@@ -49,7 +51,9 @@ typedef enum conv_detection {
 	TARGET_VCARD_30 = 2,
 	TARGET_VEVENT_10 = 3,
 	TARGET_VEVENT_20 = 4,
-	TARGET_XMLFORMAT = 5
+	TARGET_VTODO_10 = 5,
+	TARGET_VTODO_20 = 6,
+	TARGET_XMLFORMAT = 7
 } conv_detection;
 
 OSyncObjFormat *conv_run_detection(OSyncFormatEnv *env, OSyncChange *change, conv_detection type)
@@ -128,6 +132,36 @@ OSyncObjFormat *conv_run_detection(OSyncFormatEnv *env, OSyncChange *change, con
 		goto out;
 	}
 
+	if (!strcmp(osync_objformat_get_name(sourceformat), "vtodo10")) {
+		switch (type) {
+		case TARGET_AUTO:
+		case TARGET_VTODO_20:
+			targetformat = osync_format_env_find_objformat(env, "vtodo20");
+			break;
+		case TARGET_XMLFORMAT:
+			targetformat = osync_format_env_find_objformat(env, "xmlformat-todo");
+			break;
+		default:
+			fprintf(stderr, "Unable to convert vtodo10 into this format. Supported formats: xmlformat, vtodo20\n");
+		}
+		goto out;
+	}
+
+	if (!strcmp(osync_objformat_get_name(sourceformat), "vtodo20")) {
+		switch (type) {
+		case TARGET_AUTO:
+		case TARGET_VTODO_10:
+			targetformat = osync_format_env_find_objformat(env, "vtodo10");
+			break;
+		case TARGET_XMLFORMAT:
+			targetformat = osync_format_env_find_objformat(env, "xmlformat-todo");
+			break;
+		default:
+			fprintf(stderr, "Unable to convert vtodo20 into this format. Supported formats: xmlformat, vtodo10\n");
+		}
+		goto out;
+	}
+
 	fprintf(stderr, "Cannot convert objtype %s. Unable to find a converter\n", osync_objformat_get_name(sourceformat));
 out:
 	return targetformat;
@@ -154,6 +188,10 @@ int main (int argc, char *argv[])
 			type = TARGET_VEVENT_10;
 		} else if (!strcmp (arg, "--to-vevent20")) {
 			type = TARGET_VEVENT_20;
+		} else if (!strcmp (arg, "--to-vtodo10")) {
+			type = TARGET_VTODO_10;
+		} else if (!strcmp (arg, "--to-vtodo20")) {
+			type = TARGET_VTODO_20;
 		} else if (!strcmp (arg, "--to-xmlformat")) {
 			type = TARGET_XMLFORMAT;
 		} else if (!strcmp (arg, "--out")) {
