d41b501f2d5ba889aac9c7e4ce52b3bbba28400d
dgollub <dgollub>
2007-07-04 14:55:26 +0000

    Implemented xmlformat-$objtype-doc in plain format detector. This is very helpful if you are going to write  a converter from xmlformat-$objtype to $yourformat.
    See vconvert tool in vformat for example implementation.
    
    You only have to read a plain string dump of a xmlformat and let
    osync_format_env_detect_objformat_full() detect the correct xmlformat
    object format.

--- formats/xmlformat-doc.c
+++ formats/xmlformat-doc.c
@@ -26,6 +26,45 @@
 #include <opensync/opensync-merger.h>
 #include <opensync/opensync-format.h>
 
+static osync_bool detect_plain_as_xmlformat(const char *objtype, const char *data, int size)
+{
+	osync_assert(objtype);
+
+	OSyncXMLFormat *xmlformat = osync_xmlformat_parse(data, size, NULL);
+	if (!xmlformat)
+		return FALSE;
+
+	osync_xmlformat_unref(xmlformat);
+
+	int ret = strcmp(objtype, osync_xmlformat_get_objtype(xmlformat));
+
+	if (ret)
+		return FALSE;
+
+
+	return TRUE;
+}
+
+static osync_bool detect_plain_as_xmlformat_contact(const char *data, int size)
+{
+	return detect_plain_as_xmlformat("contact", data, size);
+}
+
+static osync_bool detect_plain_as_xmlformat_event(const char *data, int size)
+{
+	return detect_plain_as_xmlformat("event", data, size);
+}
+
+static osync_bool detect_plain_as_xmlformat_todo(const char *data, int size)
+{
+	return detect_plain_as_xmlformat("todo", data, size);
+}
+
+static osync_bool detect_plain_as_xmlformat_note(const char *data, int size)
+{
+	return detect_plain_as_xmlformat("note", data, size);
+}
+
 static osync_bool from_xml(char *input, unsigned int inpsize, char **output, unsigned int *outpsize, osync_bool *free_input, const char *config, OSyncError **error)
 {
 	*free_input = TRUE;
@@ -67,26 +106,34 @@ static osync_bool register_format(OSyncFormatEnv *env, const char *name, const c
 
 void get_format_info(OSyncFormatEnv *env)
 {
+	if (!register_format(env, "xmlformat-contact-doc", "contact"))
+		return;
+
 	if (!register_format(env, "xmlformat-event-doc", "event"))
 		return;
 
-	if (!register_format(env, "xmlformat-contact-doc", "contact"))
+	if (!register_format(env, "xmlformat-todo-doc", "todo"))
+		return;
+
+	if (!register_format(env, "xmlformat-note-doc", "note"))
 		return;
 }
 
-static osync_bool register_converter(OSyncFormatEnv *env, const char *fromname, const char *toname)
+static osync_bool register_converter(OSyncFormatEnv *env, const char *fromname, const char *toname, OSyncFormatDetectFunc detect_func)
 {
 	OSyncFormatConverter *conv;
 	OSyncError *error = NULL;
 
 	OSyncObjFormat *fromformat = osync_format_env_find_objformat(env, fromname);
 	OSyncObjFormat *toformat = osync_format_env_find_objformat(env, toname);
+	OSyncObjFormat *plain = osync_format_env_find_objformat(env, "plain");
 
 	if (!fromformat || !toformat) {
 		osync_trace(TRACE_ERROR, "Unable to register converter for %s->%s, format not found\n", fromname, toname);
 		return FALSE;
 	}
 
+	/* Converters */
 	conv = osync_converter_new(OSYNC_CONVERTER_CONV, fromformat, toformat, to_xml, &error);
 	if (!conv) {
 		osync_trace(TRACE_ERROR, "Unable to register converter: %s", osync_error_print(&error));
@@ -105,16 +152,35 @@ static osync_bool register_converter(OSyncFormatEnv *env, const char *fromname, 
 	osync_format_env_register_converter(env, conv);
 	osync_converter_unref(conv);
 
+
+	/* Detector: plain as xmlformat */
+	conv = osync_converter_new_detector(plain, fromformat, detect_func, &error);
+	if (!conv) {
+		osync_trace(TRACE_ERROR, "Unable to register detector: %s", osync_error_print(&error));
+		osync_error_unref(&error);
+		return FALSE;
+	}
+	osync_format_env_register_converter(env, conv);
+	osync_converter_unref(conv);
+
 	return TRUE;
 }
 
-void get_conversion_info(OSyncFormatEnv *env)
+osync_bool get_conversion_info(OSyncFormatEnv *env)
 {
-	if (!register_converter(env, "xmlformat-event-doc", "xmlformat-event"))
-		return;
+	if (!register_converter(env, "xmlformat-contact-doc", "xmlformat-contact", detect_plain_as_xmlformat_contact))
+		return FALSE;
 
-	if (!register_converter(env, "xmlformat-contact-doc", "xmlformat-contact"))
-		return;
+	if (!register_converter(env, "xmlformat-event-doc", "xmlformat-event", detect_plain_as_xmlformat_event))
+		return FALSE;
+
+	if (!register_converter(env, "xmlformat-todo-doc", "xmlformat-todo", detect_plain_as_xmlformat_todo))
+		return FALSE;
+
+	if (!register_converter(env, "xmlformat-note-doc", "xmlformat-note", detect_plain_as_xmlformat_note))
+		return FALSE;
+
+	return TRUE;
 }
 
 int get_version(void)
