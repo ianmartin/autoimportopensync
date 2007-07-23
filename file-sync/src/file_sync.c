3a465a56497f41d416a38bdab821dbf7eee936d5
dgollub <dgollub>
2007-07-23 13:32:13 +0000

    Fixed crash if <objformat> isn't set. Fixed memory leak in freeing directory (objformat was leaking)

--- file-sync/src/file_sync.c
+++ file-sync/src/file_sync.c
@@ -30,6 +30,10 @@ static void free_dir(OSyncFileDir *dir)
 		
 	if (dir->objtype)
 		g_free(dir->objtype);
+
+	if (dir->objformat)
+		g_free(dir->objformat);
+
 	
 	g_free(dir);
 }
@@ -695,6 +699,8 @@ static void *osync_filesync_initialize(OSyncPlugin *plugin, OSyncPluginInfo *inf
 		if (!sink)
 			goto error_free_env;
 		
+		if (!dir->objformat)
+			dir->objformat = g_strdup("file");
 
 		if (!osync_format_env_find_objformat(formatenv, dir->objformat)) {
 			osync_error_set(error, OSYNC_ERROR_GENERIC, "Configured storage format \"%s\" for object type \"%s\" is unknown. Is the format plugin missing?");
@@ -704,7 +710,6 @@ static void *osync_filesync_initialize(OSyncPlugin *plugin, OSyncPluginInfo *inf
 
 		dir->sink = sink;
 		
-		//osync_objtype_sink_add_objformat(sink, "file");
 		osync_objtype_sink_add_objformat(sink, dir->objformat);
 		
 		/* All sinks have the same functions of course */
