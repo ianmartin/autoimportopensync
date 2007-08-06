1c2797df1c768eb92e790842e1b9a255c8790dd5
cstender <cstender>
2007-08-06 12:27:40 +0000

    changed license of all files to LGPL as discussed with armin

--- vformat/tools/vconvert.c
+++ vformat/tools/vconvert.c
@@ -2,22 +2,27 @@
  * vconvert - A tool for converting vcards, vevents, vtodos and vnotes
  * Copyright (C) 2004-2005  Armin Bauer <armin.bauer@opensync.org>
  * Copyright (C) 2007  Christopher Stender <cstender@suse.de>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
- * 
+ *
+ * Author: Chris Toshok (toshok@ximian.com)
+ * Author: Armin Bauer (armin.bauer@opensync.org)
+ * Author: Christopher Stender (cstender@suse.de)
+ *
  */
+
 
 #include <opensync/opensync.h>
 #include <opensync/opensync-format.h>
