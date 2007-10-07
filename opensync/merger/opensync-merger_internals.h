77b32dfff8c8123e39a70bfc19b53ce8346f6f79
dgollub <dgollub>
2007-10-07 11:57:02 +0000

    Remove unneeded header.

--- opensync/merger/opensync-merger_internals.h
+++ opensync/merger/opensync-merger_internals.h
@@ -23,9 +23,6 @@
 #ifndef OPENSYNCMERGER_INTERNALS_H_
 #define OPENSYNCMERGER_INTERNALS_H_
 
-#include <sqlite3.h>
-#include <libxml/parser.h>
-#include <libxml/xmlschemas.h>
 #include <opensync/opensync_xml.h>
 
 #include "opensync_capabilities_internals.h"
