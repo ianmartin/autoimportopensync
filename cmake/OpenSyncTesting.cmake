609d1b05b7d21a5368a8e67ab61660cf38ffd18a
dgollub <dgollub>
2007-11-02 18:37:07 +0000

    Allow serveral target libraries as last argument. Unfortunately only one testcase sources file is allowed for now...

--- cmake/OpenSyncTesting.cmake
+++ cmake/OpenSyncTesting.cmake
@@ -1,10 +1,10 @@
 ENABLE_TESTING()
 INCLUDE( CTest )
 
-MACRO( OPENSYNC_ADD_TEST _testName _testSource _targetLibraries ) 
+MACRO( OPENSYNC_ADD_TEST _testName _testSource ) 
 
   ADD_EXECUTABLE( ${_testName} ${_testSource} )
-  TARGET_LINK_LIBRARIES( ${_testName} ${_targetLibraries} )
+  TARGET_LINK_LIBRARIES( ${_testName} ${ARGN} )
   ADD_TEST( ${_testName} ${CMAKE_CURRENT_BINARY_DIR}/${_testName} )
 
 ENDMACRO( OPENSYNC_ADD_TEST )
