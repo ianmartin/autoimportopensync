255590fa0b83dd5e29b983eae9bcf2a9c95f0f81
dgollub <dgollub>
2007-11-04 18:31:11 +0000

    Make use of the OpenSyncPlugin module even for the shipped plugins: example plugin, mock plugin, and the basic format plugins.
    Add option to enable/disable build of OPENSYNC_PYTHONBINDINGS. It got a
    bit refacotred so we can use swig also to generate other bindings ...
    later.

--- cmake/OpenSyncDefaults.cmake
+++ cmake/OpenSyncDefaults.cmake
@@ -47,4 +47,6 @@ ENDIF ( NOT CMAKE_BUILD_TYPE )
 SET( OPENSYNC_TRACE TRUE CACHE BOOL "Debugging/Trace output of OpenSync" )
 SET( OPENSYNC_DEBUG_MODULES FALSE CACHE BOOL "Debugging modules. Avhoid unload of modules." )
 SET( OPENSYNC_UNITTESTS FALSE CACHE BOOL "Build OpenSync unit tests." )
+SET( OPENSYNC_PYTHONBINDINGS TRUE CACHE BOOL "Build OpenSync with Python bindings." )
+
 
