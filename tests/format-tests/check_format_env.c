db4819189352bb005060c403e0a61787e334ba8f
dgollub <dgollub>
2007-11-05 22:14:22 +0000

    Make the unit tests module and format_env happy again. mock plugin module got renamend. (module prefix is gone when using OPENSYNC_FORMAT_ADD) for mock plugins.

--- tests/format-tests/check_format_env.c
+++ tests/format-tests/check_format_env.c
@@ -369,7 +369,7 @@ START_TEST (conv_env_plugin)
 	fail_unless(error == NULL, NULL);
 	
 	char *curdir = g_get_current_dir();
-	char *path = g_strdup_printf("%s/libmock-format.so", curdir);
+	char *path = g_strdup_printf("%s/mock-format.so", curdir);
 	fail_unless(osync_module_load(module, path, &error), NULL);
 	fail_unless(error == NULL, NULL);
 	g_free(path);
