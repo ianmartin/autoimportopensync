cddb536ac1233ca38cf3f796ae2c0c124d256d20
dgollub <dgollub>
2007-11-11 00:38:57 +0000

    Added profiling flags.

--- cmake/OpenSyncTesting.cmake
+++ cmake/OpenSyncTesting.cmake
@@ -1,6 +1,12 @@
 ENABLE_TESTING()
 INCLUDE( CTest )
 
+SET( CMAKE_BUILD_TYPE PROFILING )
+SET( CMAKE_C_FLAGS_PROFILING "-g -O0 -Wall -W -Wshadow -Wunused-variable -Wunused-parameter -Wunused-function -Wunused -Wno-system-headers -Wwrite-strings -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Compiler Flags" ) 
+SET( CMAKE_SHARED_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags" )
+SET( CMAKE_MODULE_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags" )
+SET( CMAKE_EXEC_LINKER_FLAGS_PROFILING " -fprofile-arcs -ftest-coverage" CACHE STRING "Profiling Linker Flags" )
+
 MACRO( OPENSYNC_ADD_TEST _testName _testSource ) 
 
   ADD_EXECUTABLE( ${_testName} ${_testSource} )
