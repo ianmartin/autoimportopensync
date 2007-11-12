b9a6fa9379525a38996ff2f577b685fb41cc821c
dgollub <dgollub>
2007-11-12 15:35:34 +0000

    Build for unit test opensync-testing, a shared lib without hidden symbols. So we can even test nnot public interfaces...

--- cmake/Compiler.cmake
+++ cmake/Compiler.cmake
@@ -1,13 +1,13 @@
 # Copyright (c) 2007 Daniel Gollub <dgollub@suse.de>
 
 IF ( WIN32 )
-	#ADD_DEFINITIONS(  )
+	SET( SYMBOLS_VISIBILITY "" )
 ENDIF ( WIN32 )
 
 IF ( CMAKE_COMPILER_IS_GNUCC ) 
-	ADD_DEFINITIONS( -fvisibility=hidden )
+	SET( SYMBOLS_VISIBILITY "-fvisibility=hidden" )
 ENDIF ( CMAKE_COMPILER_IS_GNUCC ) 
 
 IF (CMAKE_SYSTEM MATCHES "SunOS-5*.")	
-	ADD_DEFINITIONS( -xldscope=hidden )
+	SET( SYMBOLS_VISIBILITY "-xldscope=hidden" )
 ENDIF (CMAKE_SYSTEM MATCHES "SunOS-5*.")	
