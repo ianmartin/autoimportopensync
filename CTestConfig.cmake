769ad1ab5d5b43e3294a352c5a9f24e555f54a99
dgollub <dgollub>
2007-12-08 23:06:50 +0000

    Added dart server config. Commit test results with:
    make ExperimentalStart
    make ExperimentalBuild
    make ExperimentalTest
    make ExperimentalSubmit
    
    or just
    make Experimental

---
+++ CTestConfig.cmake
@@ -0,0 +1,6 @@
+SET (CTEST_PROJECT_NAME "OpenSync" )
+SET (CTEST_DROP_METHOD "xmlrpc" )
+SET (CTEST_DROP_SITE "http://opensync.org/")
+SET (CTEST_DROP_LOCATION "OpenSync")
+SET (COMPRESS_SUBMISSON ON)
+#SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-OpenSync-TestingResults.pl")
