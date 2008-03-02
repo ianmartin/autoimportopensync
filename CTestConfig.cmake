793be132559515328811e3f9bfc0851f49a9a7a1
dgollub <dgollub>
2008-03-02 00:09:11 +0000

    Adated CTestConfig.cmake to new dashboard system CDash. Byebye Dart2...
    http://opensync.org/testing/

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,6 @@
-SET (CTEST_PROJECT_NAME "OpenSync" )
-SET (CTEST_DROP_METHOD "xmlrpc" )
-SET (CTEST_DROP_SITE "http://opensync.org/")
-SET (CTEST_DROP_LOCATION "OpenSync")
-SET (COMPRESS_SUBMISSON ON)
-#SET (CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-OpenSync-TestingResults.pl")
+set(CTEST_PROJECT_NAME "OpenSync")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "opensync.org")
+set(CTEST_DROP_LOCATION "/testing/submit.php?project=OpenSync")
+set(CTEST_DROP_SITE_CDASH TRUE)
