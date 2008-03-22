8b108f04cdd52976660bf0ac7ee87ce1800d6448
dgollub <dgollub>
2008-03-22 16:19:43 +0000

    Improved testing. Code coverage of 100% for the new plugin config cmponents.

--- tests/plugin-tests/check_plugin_config.c
+++ tests/plugin-tests/check_plugin_config.c
@@ -58,7 +58,7 @@ START_TEST (plugin_config_subcomponents)
 	fail_unless(error == NULL, NULL);
 	fail_unless(conn != NULL, NULL);
 	osync_plugin_config_set_connection(config, conn);
-	fail_unless(osync_plugin_config_get_connection(config) != NULL, NULL);
+	fail_unless(osync_plugin_config_get_connection(config) == conn, NULL);
 	fail_unless(osync_plugin_connection_ref(conn) != NULL, NULL);
 	osync_plugin_connection_unref(conn);
 	osync_plugin_connection_unref(conn);
@@ -68,7 +68,7 @@ START_TEST (plugin_config_subcomponents)
 	fail_unless(error == NULL, NULL);
 	fail_unless(local != NULL, NULL);
 	osync_plugin_config_set_localization(config, local);
-	fail_unless(osync_plugin_config_get_localization(config) != NULL, NULL);
+	fail_unless(osync_plugin_config_get_localization(config) == local, NULL);
 	fail_unless(osync_plugin_localization_ref(local) != NULL, NULL);
 	osync_plugin_localization_unref(local);
 	osync_plugin_localization_unref(local);
@@ -78,10 +78,36 @@ START_TEST (plugin_config_subcomponents)
 	fail_unless(error == NULL, NULL);
 	fail_unless(auth != NULL, NULL);
 	osync_plugin_config_set_authentication(config, auth);
-	fail_unless(osync_plugin_config_get_authentication(config) != NULL, NULL);
+	fail_unless(osync_plugin_config_get_authentication(config) == auth, NULL);
 	fail_unless(osync_plugin_authentication_ref(auth) != NULL, NULL);
 	osync_plugin_authentication_unref(auth);
 	osync_plugin_authentication_unref(auth);
+
+
+	/* Overwrite Subcomponents */
+	/* Connection */
+	OSyncPluginConnection *conn2 = osync_plugin_connection_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn2 != NULL, NULL);
+	osync_plugin_config_set_connection(config, conn2);
+	fail_unless(osync_plugin_config_get_connection(config) == conn2, NULL);
+	osync_plugin_connection_unref(conn2);
+
+	/* Localization */
+	OSyncPluginLocalization *local2 = osync_plugin_localization_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(local2 != NULL, NULL);
+	osync_plugin_config_set_localization(config, local2);
+	fail_unless(osync_plugin_config_get_localization(config) == local2, NULL);
+	osync_plugin_localization_unref(local2);
+
+	/* Authentication */
+	OSyncPluginAuthentication *auth2 = osync_plugin_authentication_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(auth2 != NULL, NULL);
+	osync_plugin_config_set_authentication(config, auth2);
+	fail_unless(osync_plugin_config_get_authentication(config) == auth2, NULL);
+	osync_plugin_authentication_unref(auth2);
 
 	osync_plugin_config_unref(config);
 
@@ -125,6 +151,218 @@ START_TEST (plugin_config_subcomponents_nomemory)
 }
 END_TEST
 
+START_TEST (plugin_config_authentication)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Authentication */
+	OSyncPluginAuthentication *auth = osync_plugin_authentication_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(auth != NULL, NULL);
+
+	osync_plugin_authentication_set_username(auth, "foo");
+	fail_unless(!strcmp(osync_plugin_authentication_get_username(auth), "foo"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_authentication_set_username(auth, "bar");
+	fail_unless(!strcmp(osync_plugin_authentication_get_username(auth), "bar"), NULL);
+
+	osync_plugin_authentication_set_password(auth, "foo");
+	fail_unless(!strcmp(osync_plugin_authentication_get_password(auth), "foo"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_authentication_set_password(auth, "bar");
+	fail_unless(!strcmp(osync_plugin_authentication_get_password(auth), "bar"), NULL);
+
+	osync_plugin_authentication_set_reference(auth, "foo");
+	fail_unless(!strcmp(osync_plugin_authentication_get_reference(auth), "foo"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_authentication_set_reference(auth, "bar");
+	fail_unless(!strcmp(osync_plugin_authentication_get_reference(auth), "bar"), NULL);
+
+	fail_unless(osync_plugin_authentication_ref(auth) != NULL, NULL);
+	osync_plugin_authentication_unref(auth);
+	osync_plugin_authentication_unref(auth);
+
+	osync_plugin_config_unref(config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_connection)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+	/* Bluetooth Address */
+	osync_plugin_connection_bt_set_addr(conn, "FF:FF:FF:FF:FF:FF");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_addr(conn), "FF:FF:FF:FF:FF:FF"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_connection_bt_set_addr(conn, "AA:FF:EE:AA:FF:EE");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_addr(conn), "AA:FF:EE:AA:FF:EE"), NULL);
+
+	/* Bluetooth Channel */
+	osync_plugin_connection_bt_set_channel(conn, 11);
+	fail_unless(osync_plugin_connection_bt_get_channel(conn) == 11, NULL);
+
+	/* Overwrite */
+	osync_plugin_connection_bt_set_channel(conn, 2);
+	fail_unless(osync_plugin_connection_bt_get_channel(conn) == 2, NULL);
+
+	/* Bluetooth SDP UUID */
+	osync_plugin_connection_bt_set_sdpuuid(conn, "00000001-0000-1000-8000-0002EE000002");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_sdpuuid(conn), "00000001-0000-1000-8000-0002EE000002"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_connection_bt_set_sdpuuid(conn, "AAFFEE");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_sdpuuid(conn), "AAFFEE"), NULL);
+
+	/* Bluetooth SDP UUID */
+	osync_plugin_connection_bt_set_sdpuuid(conn, "00000001-0000-1000-8000-0002EE000002");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_sdpuuid(conn), "00000001-0000-1000-8000-0002EE000002"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_connection_bt_set_sdpuuid(conn, "AAFFEE");
+	fail_unless(!strcmp(osync_plugin_connection_bt_get_sdpuuid(conn), "AAFFEE"), NULL);
+
+	/* USB Vendor ID */
+	osync_plugin_connection_usb_set_vendorid(conn, 0xffff);
+	fail_unless(osync_plugin_connection_usb_get_vendorid(conn) == 0xffff, NULL);
+
+	osync_plugin_connection_usb_set_vendorid(conn, 0xaffe);
+	fail_unless(osync_plugin_connection_usb_get_vendorid(conn) == 0xaffe, NULL);
+
+	/* USB Product ID */
+	osync_plugin_connection_usb_set_productid(conn, 0xffff);
+	fail_unless(osync_plugin_connection_usb_get_productid(conn) == 0xffff, NULL);
+
+	osync_plugin_connection_usb_set_productid(conn, 0xaffe);
+	fail_unless(osync_plugin_connection_usb_get_productid(conn) == 0xaffe, NULL);
+
+	/* Network Address */
+	osync_plugin_connection_net_set_address(conn, "opensync.org");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_address(conn), "opensync.org"), NULL);
+
+	osync_plugin_connection_net_set_address(conn, "libsyncml.opensync.org");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_address(conn), "libsyncml.opensync.org"), NULL);
+
+	/* Network Port */
+	osync_plugin_connection_net_set_port(conn, 8888);
+	fail_unless(osync_plugin_connection_net_get_port(conn) == 8888, NULL);
+
+	osync_plugin_connection_net_set_port(conn, 9999);
+	fail_unless(osync_plugin_connection_net_get_port(conn) == 9999, NULL);
+
+	/* Network Protocol */
+	osync_plugin_connection_net_set_protocol(conn, "http://");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_protocol(conn), "http://"), NULL);
+
+	osync_plugin_connection_net_set_protocol(conn, "ssh://");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_protocol(conn), "ssh://"), NULL);
+
+	/* Network DNSSD */
+	osync_plugin_connection_net_set_dnssd(conn, "_syncml-http._tcp");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_dnssd(conn), "_syncml-http._tcp"), NULL);
+
+	osync_plugin_connection_net_set_dnssd(conn, "_syncml-obex._tcp");
+	fail_unless(!strcmp(osync_plugin_connection_net_get_dnssd(conn), "_syncml-obex._tcp"), NULL);
+
+	/* Serial Speed */
+	osync_plugin_connection_serial_set_speed(conn, 1234);
+	fail_unless(osync_plugin_connection_serial_get_speed(conn) == 1234, NULL);
+
+	osync_plugin_connection_serial_set_speed(conn, 4321);
+	fail_unless(osync_plugin_connection_serial_get_speed(conn) == 4321, NULL);
+
+	/* Serial Devicenode */
+	osync_plugin_connection_serial_set_devicenode(conn, "/dev/ttyS0");
+	fail_unless(!strcmp(osync_plugin_connection_serial_get_devicenode(conn), "/dev/ttyS0"), NULL);
+
+	osync_plugin_connection_serial_set_devicenode(conn, "/dev/ttyUSB0");
+	fail_unless(!strcmp(osync_plugin_connection_serial_get_devicenode(conn), "/dev/ttyUSB0"), NULL);
+
+	/* IrDA Service */
+	osync_plugin_connection_irda_set_service(conn, "FancyIR Mobile");
+	fail_unless(!strcmp(osync_plugin_connection_irda_get_service(conn), "FancyIR Mobile"), NULL);
+
+	osync_plugin_connection_irda_set_service(conn, "IRMobile");
+	fail_unless(!strcmp(osync_plugin_connection_irda_get_service(conn), "IRMobile"), NULL);
+
+	fail_unless(osync_plugin_connection_ref(conn) != NULL, NULL);
+	osync_plugin_connection_unref(conn);
+	osync_plugin_connection_unref(conn);
+
+	osync_plugin_config_unref(config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_localization)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Localization */
+	OSyncPluginLocalization *local = osync_plugin_localization_new(&error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(local != NULL, NULL);
+
+	/* Encoding */
+	osync_plugin_localization_set_encoding(local, "cp1222");
+	fail_unless(!strcmp(osync_plugin_localization_get_encoding(local), "cp1222"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_localization_set_encoding(local, "cp2221");
+	fail_unless(!strcmp(osync_plugin_localization_get_encoding(local), "cp2221"), NULL);
+
+	/* Timezone */
+	osync_plugin_localization_set_timezone(local, "Europe/Berlin");
+	fail_unless(!strcmp(osync_plugin_localization_get_timezone(local), "Europe/Berlin"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_localization_set_timezone(local, "Europe/Munich");
+	fail_unless(!strcmp(osync_plugin_localization_get_timezone(local), "Europe/Munich"), NULL);
+
+	/* Language */
+	osync_plugin_localization_set_language(local, "de_DE");
+	fail_unless(!strcmp(osync_plugin_localization_get_language(local), "de_DE"), NULL);
+
+	/* Overwrite (leak check) */
+	osync_plugin_localization_set_language(local, "en_US");
+	fail_unless(!strcmp(osync_plugin_localization_get_language(local), "en_US"), NULL);
+
+	fail_unless(osync_plugin_localization_ref(local) != NULL, NULL);
+	osync_plugin_localization_unref(local);
+	osync_plugin_localization_unref(local);
+
+	osync_plugin_config_unref(config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
 
 Suite *client_suite(void)
 {
@@ -135,7 +373,9 @@ Suite *client_suite(void)
 	create_case(s, "plugin_config_new_nomemory", plugin_config_new_nomemory);
 	create_case(s, "plugin_config_subcomponents", plugin_config_subcomponents);
 	create_case(s, "plugin_config_subcomponents_nomemory", plugin_config_subcomponents_nomemory);
-
+	create_case(s, "plugin_config_authentication", plugin_config_authentication);
+	create_case(s, "plugin_config_connection", plugin_config_connection);
+	create_case(s, "plugin_config_localization", plugin_config_localization);
 	
 	return s;
 }
