6383912d937dd6c81b66a7d11d0218e719dfdfe6
dgollub <dgollub>
2008-04-06 13:46:15 +0000

    More plugin connection configuration specific testcases for assembling and parsing the configuration.
    TODO: Testcases coverage missing some error conditions, beside nomemory.

--- tests/plugin-tests/check_plugin_config.c
+++ tests/plugin-tests/check_plugin_config.c
@@ -393,20 +393,18 @@ START_TEST (plugin_config_save_and_load)
 	/* Language */
 	osync_plugin_localization_set_language(local, "de_DE");
 
-	/* Connection */
-	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_BLUETOOTH, &error);
+	/* Authentication */
+	OSyncPluginAuthentication *auth = osync_plugin_authentication_new(&error);
 	fail_unless(error == NULL, NULL);
-	fail_unless(conn != NULL, NULL);
+	fail_unless(auth != NULL, NULL);
 
-	/* Bluetooth Address */
-	osync_plugin_connection_bt_set_addr(conn, "FF:FF:FF:FF:FF:FF");
-
-	/* Bluetooth SDP UUID */
-	osync_plugin_connection_bt_set_sdpuuid(conn, "00000001-0000-1000-8000-0002EE000002");
+	osync_plugin_authentication_set_username(auth, "foo");
+	osync_plugin_authentication_set_password(auth, "bar");
+	osync_plugin_authentication_set_reference(auth, "ref");
 
 	/* Set subcomponents */
-	osync_plugin_config_set_connection(config, conn);
-	osync_plugin_connection_unref(conn);
+	osync_plugin_config_set_authentication(config, auth);
+	osync_plugin_authentication_unref(auth);
 	osync_plugin_config_set_localization(config, local);
 	osync_plugin_localization_unref(local);
 
@@ -417,18 +415,259 @@ START_TEST (plugin_config_save_and_load)
 
 	/* Compare stored config with original config */
 
-	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
 	OSyncPluginLocalization *reloaded_local = osync_plugin_config_get_localization(reloaded_config);
+	OSyncPluginAuthentication *reloaded_auth = osync_plugin_config_get_authentication(reloaded_config);
 
-	fail_unless(reloaded_conn != NULL, NULL);
 	fail_unless(reloaded_local != NULL, NULL);
 
 	fail_unless(!strcmp(osync_plugin_localization_get_language(reloaded_local), "de_DE"), NULL);
 	fail_unless(!strcmp(osync_plugin_localization_get_encoding(reloaded_local), "cp1222"), NULL);
 	fail_unless(!strcmp(osync_plugin_localization_get_timezone(reloaded_local), "Europe/Berlin"), NULL);
 
+	fail_unless(!strcmp(osync_plugin_authentication_get_username(reloaded_auth), "foo"), NULL);
+	fail_unless(!strcmp(osync_plugin_authentication_get_password(reloaded_auth), "bar"), NULL);
+	fail_unless(!strcmp(osync_plugin_authentication_get_reference(reloaded_auth), "ref"), NULL);
+
+
+	osync_plugin_config_unref(config);
+	osync_plugin_config_unref(reloaded_config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_save_and_load_connection_bluetooth)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	OSyncPluginConfig *reloaded_config = osync_plugin_config_new(&error); 
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_BLUETOOTH, &error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+	/* Bluetooth Address */
+	osync_plugin_connection_bt_set_addr(conn, "FF:FF:FF:FF:FF:FF");
+
+	/* Bluetooth RFCOMM */
+	osync_plugin_connection_bt_set_channel(conn, 11);
+
+	/* Bluetooth SDP UUID */
+	osync_plugin_connection_bt_set_sdpuuid(conn, "00000001-0000-1000-8000-0002EE000002");
+
+
+	/* Set subcomponents */
+	osync_plugin_config_set_connection(config, conn);
+	osync_plugin_connection_unref(conn);
+
+	char *config_file = g_strdup_printf("%s/dummy_config.xml", testbed);
+	fail_unless(osync_plugin_config_file_save(config, config_file, &error), "%s", osync_error_print(&error));
+	fail_unless(_osync_plugin_config_file_load(reloaded_config, config_file, testbed, &error), NULL);
+	g_free(config_file);
+
+	/* Compare stored config with original config */
+
+	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
+
+	fail_unless(reloaded_conn != NULL, NULL);
+
 	fail_unless(!strcmp(osync_plugin_connection_bt_get_sdpuuid(reloaded_conn), "00000001-0000-1000-8000-0002EE000002"), NULL);
+	fail_unless(osync_plugin_connection_bt_get_channel(reloaded_conn) == 11, NULL);
 	fail_unless(!strcmp(osync_plugin_connection_bt_get_addr(reloaded_conn), "FF:FF:FF:FF:FF:FF"), NULL);
+
+	osync_plugin_config_unref(config);
+	osync_plugin_config_unref(reloaded_config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_save_and_load_connection_irda)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	OSyncPluginConfig *reloaded_config = osync_plugin_config_new(&error); 
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_IRDA, &error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+	osync_plugin_connection_irda_set_service(conn, "FancyIR Mobile");
+
+	/* Set subcomponents */
+	osync_plugin_config_set_connection(config, conn);
+	osync_plugin_connection_unref(conn);
+
+	char *config_file = g_strdup_printf("%s/dummy_config.xml", testbed);
+	fail_unless(osync_plugin_config_file_save(config, config_file, &error), "%s", osync_error_print(&error));
+	fail_unless(_osync_plugin_config_file_load(reloaded_config, config_file, testbed, &error), NULL);
+	g_free(config_file);
+
+	/* Compare stored config with original config */
+
+	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
+
+	fail_unless(reloaded_conn != NULL, NULL);
+
+	fail_unless(!strcmp(osync_plugin_connection_irda_get_service(reloaded_conn), "FancyIR Mobile"), NULL);
+
+	osync_plugin_config_unref(config);
+	osync_plugin_config_unref(reloaded_config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_save_and_load_connection_usb)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	OSyncPluginConfig *reloaded_config = osync_plugin_config_new(&error); 
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_USB, &error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+	/* USB Vendor ID */
+	osync_plugin_connection_usb_set_vendorid(conn, 0xaffe);
+
+	/* USB Product ID */
+	osync_plugin_connection_usb_set_productid(conn, 0xffff);
+
+	/* USB Interface*/
+	osync_plugin_connection_usb_set_interface(conn, 2);
+
+	/* Set subcomponents */
+	osync_plugin_config_set_connection(config, conn);
+	osync_plugin_connection_unref(conn);
+
+	char *config_file = g_strdup_printf("%s/dummy_config.xml", testbed);
+	fail_unless(osync_plugin_config_file_save(config, config_file, &error), "%s", osync_error_print(&error));
+	fail_unless(_osync_plugin_config_file_load(reloaded_config, config_file, testbed, &error), NULL);
+	g_free(config_file);
+
+	/* Compare stored config with original config */
+	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
+
+	fail_unless(reloaded_conn != NULL, NULL);
+
+	fail_unless(osync_plugin_connection_usb_get_vendorid(reloaded_conn) == 0xaffe, NULL);
+	fail_unless(osync_plugin_connection_usb_get_productid(reloaded_conn) == 0xffff, NULL);
+	fail_unless(osync_plugin_connection_usb_get_interface(reloaded_conn) == 2, NULL);
+
+	osync_plugin_config_unref(config);
+	osync_plugin_config_unref(reloaded_config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_save_and_load_connection_network)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	OSyncPluginConfig *reloaded_config = osync_plugin_config_new(&error); 
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_NETWORK, &error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+
+	/* Network Address */
+	osync_plugin_connection_net_set_address(conn, "opensync.org");
+
+	/* Network Port */
+	osync_plugin_connection_net_set_port(conn, 8888);
+
+	/* Network Protocol */
+	osync_plugin_connection_net_set_protocol(conn, "http://");
+
+	/* Network DNSSD */
+	osync_plugin_connection_net_set_dnssd(conn, "_syncml-http._tcp");
+
+	/* Set subcomponents */
+	osync_plugin_config_set_connection(config, conn);
+	osync_plugin_connection_unref(conn);
+
+	char *config_file = g_strdup_printf("%s/dummy_config.xml", testbed);
+	fail_unless(osync_plugin_config_file_save(config, config_file, &error), "%s", osync_error_print(&error));
+	fail_unless(_osync_plugin_config_file_load(reloaded_config, config_file, testbed, &error), NULL);
+	g_free(config_file);
+
+	/* Compare stored config with original config */
+	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
+
+	fail_unless(reloaded_conn != NULL, NULL);
+
+	fail_unless(!strcmp(osync_plugin_connection_net_get_dnssd(reloaded_conn), "_syncml-http._tcp"), NULL);
+	fail_unless(!strcmp(osync_plugin_connection_net_get_protocol(reloaded_conn), "http://"), NULL);
+	fail_unless(!strcmp(osync_plugin_connection_net_get_address(reloaded_conn), "opensync.org"), NULL);
+	fail_unless(osync_plugin_connection_net_get_port(reloaded_conn) == 8888, NULL);
+
+	osync_plugin_config_unref(config);
+	osync_plugin_config_unref(reloaded_config);
+
+	destroy_testbed(testbed);
+}
+END_TEST
+
+START_TEST (plugin_config_save_and_load_connection_serial)
+{
+	char *testbed = setup_testbed(NULL);
+
+	OSyncError *error = NULL;
+	OSyncPluginConfig *config = osync_plugin_config_new(&error);
+	OSyncPluginConfig *reloaded_config = osync_plugin_config_new(&error); 
+	fail_unless(error == NULL, NULL);
+	fail_unless(config != NULL, NULL);
+
+	/* Connection */
+	OSyncPluginConnection *conn = osync_plugin_connection_new(OSYNC_PLUGIN_CONNECTION_SERIAL, &error);
+	fail_unless(error == NULL, NULL);
+	fail_unless(conn != NULL, NULL);
+
+	/* Serial Speed */
+	osync_plugin_connection_serial_set_speed(conn, 1234);
+
+	/* Serial Devicenode */
+	osync_plugin_connection_serial_set_devicenode(conn, "/dev/ttyS0");
+
+	/* Set subcomponents */
+	osync_plugin_config_set_connection(config, conn);
+	osync_plugin_connection_unref(conn);
+
+	char *config_file = g_strdup_printf("%s/dummy_config.xml", testbed);
+	fail_unless(osync_plugin_config_file_save(config, config_file, &error), "%s", osync_error_print(&error));
+	fail_unless(_osync_plugin_config_file_load(reloaded_config, config_file, testbed, &error), NULL);
+	g_free(config_file);
+
+	/* Compare stored config with original config */
+	OSyncPluginConnection *reloaded_conn = osync_plugin_config_get_connection(reloaded_config);
+
+	fail_unless(reloaded_conn != NULL, NULL);
+
+	fail_unless(osync_plugin_connection_serial_get_speed(conn) == 1234, NULL);
+	fail_unless(!strcmp(osync_plugin_connection_serial_get_devicenode(conn), "/dev/ttyS0"), NULL);
 
 	osync_plugin_config_unref(config);
 	osync_plugin_config_unref(reloaded_config);
@@ -450,6 +689,11 @@ Suite *client_suite(void)
 	create_case(s, "plugin_config_connection", plugin_config_connection);
 	create_case(s, "plugin_config_localization", plugin_config_localization);
 	create_case(s, "plugin_config_save_and_load", plugin_config_save_and_load);
+	create_case(s, "plugin_config_save_and_load_connection_bluetooth", plugin_config_save_and_load_connection_bluetooth);
+	create_case(s, "plugin_config_save_and_load_connection_usb", plugin_config_save_and_load_connection_usb);
+	create_case(s, "plugin_config_save_and_load_connection_irda", plugin_config_save_and_load_connection_irda);
+	create_case(s, "plugin_config_save_and_load_connection_network", plugin_config_save_and_load_connection_network);
+	create_case(s, "plugin_config_save_and_load_connection_serial", plugin_config_save_and_load_connection_serial);
 	
 	return s;
 }
