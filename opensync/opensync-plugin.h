297e27431794a23a4f5bbd63061c0176862fd4be
dgollub <dgollub>
2008-05-03 23:32:56 +0000

    Missing commit. Include opensync_plugin_ressources.h header, fixes build.

--- opensync/opensync-plugin.h
+++ opensync/opensync-plugin.h
@@ -30,6 +30,7 @@ OPENSYNC_BEGIN_DECLS
 #include "plugin/opensync_plugin_authentication.h"
 #include "plugin/opensync_plugin_connection.h"
 #include "plugin/opensync_plugin_localization.h"
+#include "plugin/opensync_plugin_ressource.h"
 #include "plugin/opensync_sink.h"
 
 OPENSYNC_END_DECLS
