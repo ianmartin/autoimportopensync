46505d833400112a2d4bf975f9d192de1f05aa30
dgollub <dgollub>
2008-07-29 18:12:21 +0000

    Link to latest coverage results with symlink "LATEST".

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -20,6 +20,10 @@ echo $TITLE
 lcov -t "$TITLE" -b ../ -d ../ -q -c -o coverage/$TITLE.info
 genhtml --legend -t "$TITLE" -o coverage/html/$TITLE coverage/$TITLE.info &> /dev/null
 
+cd coverage/html/
+ln -sf $TITLE LATEST
+cd ../../
+
 # XXX: reset counters ( lcov -z seems to be outdated... tries to dlete .da files instead of .gcda ) 
 find ../ -name "*.gcda" | xargs rm
 
