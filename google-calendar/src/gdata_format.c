4cedadb5412a4b5a24ea78b2015e1e14b7104996
adenilsoncavalcanti <adenilsoncavalcanti>
2008-10-27 19:10:50 +0000

    Checking for parameter validity.

--- google-calendar/src/gdata_format.c
+++ google-calendar/src/gdata_format.c
@@ -107,9 +107,14 @@ void *initialize(OSyncError **error)
 
 void finalize(void *userdata)
 {
-	struct xslt_resources *converter = (struct xslt_resources *)userdata;
-	if (userdata)
-		xslt_delete(converter);
+	struct xslt_resources *converter = NULL;
+	if (!userdata)
+		return;
+	else
+		fprintf(stderr, "\n\nuserdata is not NULL: %x\n", userdata);
+
+	converter = (struct xslt_resources *)userdata;
+	xslt_delete(converter);
 }
 
 osync_bool get_conversion_info(OSyncFormatEnv *env)
