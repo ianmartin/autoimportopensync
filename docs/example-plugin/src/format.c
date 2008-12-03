1f15845bbabc8988a43179a9cddc16e8c036b541
bricks <bricks>
2008-12-03 07:09:40 +0000

    changed parameter of destroy_func1 from size_t to unsigned int

--- docs/example-plugin/src/format.c
+++ docs/example-plugin/src/format.c
@@ -4,7 +4,7 @@
  * is rather difficult or if other plugins might be able to reuse
  * your conversion. A format plugin can create new formats in get_format_info
  * and provide converters between different formats in get_conversion_info
- * 
+ *
  */
 #include <opensync/opensync.h>
 #include <opensync/opensync-format.h>
@@ -17,27 +17,27 @@ static OSyncConvCmpResult compare_format1(const char *leftdata, unsigned int lef
 	 * This function can be used to compare two types of your formats.
 	 * This is optional. For example, you can only provide a conversion
 	 * from and to the xml format and let all the comparison be done there
-	 * 
+	 *
 	 */
 
 	/*
 	 * Compare your objects here. You might need to cast the data of the change
 	 * objects to you own defined types.
-	 * 
+	 *
 	 * The possible result of the comparison are:
-	 * 
+	 *
 	 * return OSYNC_CONV_DATA_SAME;
 	 * The objects are exactly the same. (they might differ in some uid or
 	 * timestamp though, but ALL the "real" information is the same)
-	 * 
+	 *
 	 * return OSYNC_CONV_DATA_SIMILAR;
 	 * The objects are not _exactly_ the same, but they look similar. This is used
 	 * to detect conflicts. It is up to you to decide what "similar" means for your
 	 * object
-	 * 
+	 *
 	 * return OSYNC_CONV_DATA_MISMATCH;
 	 * This means the objects are not the same and not similar.
-	 * 
+	 *
 	 */
 	return OSYNC_CONV_DATA_MISMATCH;
 }
@@ -48,37 +48,37 @@ static osync_bool conv_format1_to_format2(char *input, unsigned int inpsize, cha
 	 * This function can be used to convert your format to another format.
 	 * Return TRUE if the conversion was successful or return FALSE and set
 	 * the error if something bad has happend.
-	 * 
+	 *
 	 */
-	
+
 	/* The arguments mean:
-	 * 
+	 *
 	 * - conv_data:
 	 * Pointer to the data you returned in your init function (if any)
-	 * 
+	 *
 	 * - input:
 	 * The data you need to convert
 	 * - inpsize
 	 * The size of the input data
-	 * 
+	 *
 	 * - output:
 	 * After converting you need to set this
 	 * to your result
 	 * - outpsize:
 	 * The size of the output
-	 * 
+	 *
 	 * - free_input:
 	 * You need to set this to TRUE if opensync
 	 * can free the input after the conversion (so you dont
 	 * use any reference from or to the input). A example where
 	 * *free_input = FALSE; needs to be done would be a encapsulator
 	 * that stores the input reference somewhere in its struct
-	 * 
+	 *
 	 * - error:
 	 * if something bad happens and you cannot convert, set the error!
-	 * 
+	 *
 	 */
-	
+
 	return TRUE;
 }
 
@@ -88,17 +88,17 @@ static osync_bool conv_format2_to_format1(char *input, unsigned int inpsize, cha
 	 * This function can be used to convert another format to your format.
 	 * Return TRUE if the conversion was successful or return FALSE and set
 	 * the error if something bad has happend.
-	 * 
+	 *
 	 */
-	
+
 	return TRUE;
 }
 
-static void destroy_format1(char *input, size_t inpsize)
+static void destroy_format1(char *input, unsigned int size)
 {
 	/*
 	 * Here you have to free the data allocated by your format
-	 * 
+	 *
 	 */
 }
 
@@ -109,9 +109,9 @@ static osync_bool duplicate_format1(const char *uid, const char *input, unsigned
 	 * Duplication does not mean to make 2 objects out of one,
 	 * but to change to uid of the object in such a way that it
 	 * differes from the original uid.
-	 * 
+	 *
 	 * Most format will never need this.
-	 * 
+	 *
 	 */
 
 	return TRUE;
@@ -124,15 +124,15 @@ static char *print_format1(const char *data, unsigned int size)
 	 * you have to return a human readable string here describing the object
 	 * as closely as possible. This information will be used by the user to decide
 	 * which object to pick in a conflict.
-	 * 
+	 *
 	 */
 	 return NULL;
 }
 
 osync_bool get_format_info(OSyncFormatEnv *env, OSyncError **error)
 {
-	/* 
-	 * this function is called to register a new format 
+	/*
+	 * this function is called to register a new format
 	 */
 	OSyncObjFormat *format = osync_objformat_new("<your format name>", "<some object type>", error);
 	if (!format)
@@ -178,7 +178,7 @@ osync_bool get_conversion_info(OSyncFormatEnv *env, OSyncError **error)
 	 * This function will be called directly after the plugin has been loaded
 	 * to get converters that convert between different formats
 	 */
-	
+
 	OSyncObjFormat *format1 = osync_format_env_find_objformat(env, "<your format name>");
 	if (!format1) {
 		osync_error_set(error, OSYNC_ERROR_GENERIC, "Unable to find <your format name> format");
@@ -200,7 +200,7 @@ osync_bool get_conversion_info(OSyncFormatEnv *env, OSyncError **error)
 	/* register converter */
 	osync_format_env_register_converter(env, conv);
 	osync_converter_unref(conv);
-	
+
 	conv = osync_converter_new(OSYNC_CONVERTER_CONV, format2, format1, conv_format2_to_format1, error);
 	if (!conv)
 		return FALSE;
