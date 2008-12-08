ea0f4f20a400b95527ccb3e331860d1a00963d25
paule <paule>
2008-12-08 23:17:10 +0000

    API documentation: add some missing function documentation and improve others; functions/params which are currently undocumented should not have tags (and therefore they will show up as warnings in the doxygen.log file)

--- opensync/plugin/opensync_plugin_config.h
+++ opensync/plugin/opensync_plugin_config.h
@@ -27,20 +27,33 @@
 /**
  * @defgroup OSyncPluginConfigAPI OpenSync Plugin Config
  * @ingroup OSyncPublic
- * @brief Functions to get and set a plugins configuration
+ * @brief Functions to get and set a plugin's configuration
  *
  */
 
 /*@{*/
 
+/*! @brief Flags specifying the elements supported by a plugin config
+ *
+ * @param error Pointer to and error struct
+ * @returns the newly registered plugin config object
+ */
 typedef enum {
+	/** Advanced options */
 	OPENSYNC_PLUGIN_CONFIG_ADVANCEDOPTION	= (1 << 0),
+	/** Authentication options */
 	OPENSYNC_PLUGIN_CONFIG_AUTHENTICATION	= (1 << 1),
+	/** Localization options */
 	OPENSYNC_PLUGIN_CONFIG_LOCALIZATION	= (1 << 2),
+	/** Resources */
 	OPENSYNC_PLUGIN_CONFIG_RESOURCES	= (1 << 3),
+	/** Connection options */
 	OPENSYNC_PLUGIN_CONFIG_CONNECTION	= (1 << 4)
 } OSyncPluginConfigSupportedFlag;
 
+/*! @brief Set of OSyncPluginConfigSupportedFlags
+ * 
+ **/
 typedef unsigned int OSyncPluginConfigSupportedFlags;
 
 /*! @brief Create a new plugin config object
@@ -52,13 +65,13 @@ OSYNC_EXPORT OSyncPluginConfig *osync_plugin_config_new(OSyncError **error);
 
 /*! @brief Decrease the reference count on a plugin config object
  *
- * @param Pointer to the plugin config object
+ * @param config Pointer to the plugin config object
  */
 OSYNC_EXPORT void osync_plugin_config_unref(OSyncPluginConfig *config);
 
 /*! @brief Increase the reference count on a plugin config object
  *
- * @param Pointer to the plugin config object
+ * @param config Pointer to the plugin config object
  * @returns the passed plugin config
  */
 OSYNC_EXPORT OSyncPluginConfig *osync_plugin_config_ref(OSyncPluginConfig *config);
@@ -68,7 +81,6 @@ OSYNC_EXPORT OSyncPluginConfig *osync_plugin_config_ref(OSyncPluginConfig *confi
  *
  * @param config Contents of config file as OsyncPluginConfig object
  * @param path Path to file to load
- * @param schemadir /todo
  * @param error Pointer to error struct
  * @returns True on success, False on failure - error will contain details
  */
@@ -79,20 +91,20 @@ OSYNC_EXPORT osync_bool osync_plugin_config_file_load(OSyncPluginConfig *config,
  * @param config Plugin config to save
  * @param path Path to save file
  * @param error Pointer to error struct that will contain details in case of failure
- * @returns True on succes, False on failure
+ * @returns True on success, False on failure - error will contain details
  */
 OSYNC_EXPORT osync_bool osync_plugin_config_file_save(OSyncPluginConfig *config, const char *path, OSyncError **error);
 
 
-/*! @brief
+/*! @brief Check if a config element is supported by a config
  *
  * @param config Plugin config to check
- * @param flag
- * @returns True if flag is support, False if not
+ * @param flag The type of configuration element to check
+ * @returns True if element is supported, False otherwise
  */
 OSYNC_EXPORT osync_bool osync_plugin_config_is_supported(OSyncPluginConfig *config, OSyncPluginConfigSupportedFlag flag);
 
-/*! @brief Sets the passed flags as support
+/*! @brief Sets the passed config element flags as supported by the config
  *
  * @param config An OsyncPluginConfig to modify
  * @param flags An OSyncPluginConfigSupportedFlags to set the flags to (merge or overwrite??)
@@ -102,12 +114,12 @@ OSYNC_EXPORT void osync_plugin_config_set_supported(OSyncPluginConfig *config, O
 /* Advanced Options */
 /*! @brief Get the advanced options from a config
  *
- * @param An OSyncPluginConfig to get the advanced options from
+ * @param config An OSyncPluginConfig to get the advanced options from
  * @returns An OSyncList of OSyncPluginAdvancedOption s
  */
 OSYNC_EXPORT OSyncList *osync_plugin_config_get_advancedoptions(OSyncPluginConfig *config);
 
-/*!@brief Get an OSyncPluginAdvancedOption from the config by name
+/*!@brief Get an advanced option from the config by name
  *
  * @param config An OSyncPluginConfig to search
  * @param name The name of the advanced option to get
@@ -115,16 +127,17 @@ OSYNC_EXPORT OSyncList *osync_plugin_config_get_advancedoptions(OSyncPluginConfi
  */
 OSYNC_EXPORT OSyncPluginAdvancedOption *osync_plugin_config_get_advancedoption_value_by_name(OSyncPluginConfig *config, const char *name);
 
-/*!@brief Add an OSyncPluginAdvancedOption to a config
+/*!@brief Add an advanced option to a config
  *
  * @param config The config to add the option to
+ * @param option the advanced option to add
  */
 OSYNC_EXPORT void osync_plugin_config_add_advancedoption(OSyncPluginConfig *config, OSyncPluginAdvancedOption *option);
 
-/*!@brief Remove and advanced option from a config
+/*!@brief Remove an advanced option from a config
  *
  * @param config An OSyncPluginConfig to remove from
- * @param option The OSyncPluginAdvancedOption to be removed
+ * @param option The advanced option to be removed
  */
 OSYNC_EXPORT void osync_plugin_config_remove_advancedoption(OSyncPluginConfig *config, OSyncPluginAdvancedOption *option);
 
@@ -188,10 +201,6 @@ OSYNC_EXPORT void osync_plugin_config_add_resource(OSyncPluginConfig *config, OS
  */
 OSYNC_EXPORT void osync_plugin_config_remove_resource(OSyncPluginConfig *config, OSyncPluginResource *resource);
 
-/*!@brief ?
- *
- * @param config ?
- */
 OSYNC_EXPORT void osync_plugin_config_flush_resources(OSyncPluginConfig *config);
 
 /* Connection */
